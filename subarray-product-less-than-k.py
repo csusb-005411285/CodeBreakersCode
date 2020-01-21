2bef351f7369265cdd0b93d8178cd2ff300f1037
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-01-20 17:34:06 -0800

    Update solution to Subarray Product Less Than K

--- subarray-product-less-than-k.py
+++ subarray-product-less-than-k.py
@@ -2,26 +2,19 @@ class Solution:
   def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
     if k <= 1:
       return 0
-    # define a pointer for the end of the window and the start of the window
-    start_window, end_window = 0, 0
-    # define a var to store the results
-    result = 0
-    product = 1
+    # initalize two pointers: end_window and start_window
+    end_window, start_window = 0,0
+    # initialize a variable to store the number of subarrays
+    _max, max_product = 0, 1
     # loop through the list
-    for end_window in range(len(nums)):  
-      # calculate the product of all elements 
-      product *= nums[end_window]
-      # loop until the start window and end window meet #?
-      while product >= k:
-        # calculate the product by removing one element from the start of the window 
-        product /= nums[start_window]
-        # move the start of window forward
+    for end_window in range(len(nums)):
+      # calculate the product so far
+      max_product *= nums[end_window]
+      # while the product so far is greater than or equal to k
+      while max_product >= k:
+        max_product /= nums[start_window]
         start_window += 1
       
-      # store the result
-      result += end_window - start_window + 1
+      _max += end_window - start_window + 1
       
-      end_window += 1
-  
-    return result
-      
+    return _max
