10586366f8e20f58b99a795dcacade6b01ed79c8
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-14 19:26:58 -0800

    Initial commit to Class Schedule

---
+++ class-schedule.py
@@ -0,0 +1,44 @@
+class Solution:
+  def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    """
+    :type numCourses: int
+    :type prerequisites: List[List[int]]
+    :rtype: bool
+    """
+    # init a list of empty lists
+    graph = [[] for _ in range(numCourses)]
+    # init a list to store the list of visited nodes
+    # 0 indicates node is not visited
+    visited = [0 for _ in range(numCourses)]
+    
+    # create adjacency list
+    for pair in prerequisites:
+      x, y = pair
+      graph[x].append(y)
+    
+    # visit each node
+    for i in range(numCourses):
+      if not self.dfs(graph, visited, i):
+        return False
+    return True
+    
+  def dfs(self, graph, visited, i):
+    # if ith node is marked as being visited, then a cycle is found
+    if visited[i] == -1:
+      return False
+    
+    # if it is done visted, then do not visit again
+    if visited[i] == 1:
+      return True
+    
+    # mark as **being** visited
+    visited[i] = -1
+    
+    # visit all the neighbours
+    for j in graph[i]:
+      if not self.dfs(graph, visited, j):
+        return False
+    # after visit all the neighbours, mark it as **done** visited
+    visited[i] = 1
+    
+    return True
