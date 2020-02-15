010abca3abff3268aa448be75833f0bd7f570324
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-15 11:37:43 -0800

    Solution to 802. Find Eventual Safe States
    https://leetcode.com/problems/find-eventual-safe-states/discuss/119827/20-line-Python-concise-sol-by-removing-0-out-degree-nodes

--- find-eventual-safe-states.py
+++ find-eventual-safe-states.py
@@ -1,41 +1,40 @@
 class Solution:
   def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-    graph_len = len(graph)
-    # init a list to store the outgoing edges from the node
-    edges = [0] * graph_len
-    # init a dict to store the adjacency list
-    adj_list = collections.defaultdict(list) 
+    # Problem of type graph
+    # build an adjacency list from the given information
+    # apply DFS on the graph to find nodes are eventually safe
+
+    # init a list that would act as an adj list
+    adj_list = collections.defaultdict(list)
+    # init a list that stores the number of outgoing links each node has
+    outgoing_edges = []
+    # init a list that would store the eventual safe nodes
+    eventual_safe_nodes = []
     # init a queue
     queue = []
-    ret = []
-    # loop through the graph
-    for i in range(graph_len):
-        # store the number of outgoing edges from each node
-        edges[i] = len(graph[i])
-        # if leaf node
-        if edges[i]==0:
-            # add it to the queue
-            queue.append(i)
-        # associate each node with the node connected to it.
-        # create an adjacency list
-        for j in graph[i]:
-          adj_list[j].append(i)
-    
-    # loop through the queue
+    for i in range(len(graph)):
+      outgoing_edges.append(len(graph[i]))
+      
+      # if outgoing edges are not present
+      if outgoing_edges[i] == 0:
+        # add it to the queue
+        queue.append(i)
+      
+      # build the adj list  
+      for j in graph[i]: #
+        adj_list[j].append(i) #
+
+    print(adj_list)    
+    # perform BFS iteratively
     while queue:
-      # pop the first node
-      term_node = queue.pop(0)
-      # store it in the results list
-      ret.append(term_node)
-      # loop through the adjacency list
-      # when you remove the terminal node, all the nodes connected to the
-      # terminal node become the new terminal nodes
-      for node in adj_list[term_node]:
-        # reduce the length of outgoing edges from the node 
-        edges[node] -= 1
-        # if we encounter a terminal node 
-        if edges[node]==0:
-          # add it to the queue
-          queue.append(node)
-    # return thhe sotred array
-    return sorted(ret)
+      node = queue.pop(0)
+      # then place it in the eventual safe list
+      eventual_safe_nodes.append(node)
+      
+      for n in adj_list[node]:
+        outgoing_edges[n] -= 1
+        # if you encounter a node with no outgoing links 
+        if outgoing_edges[n] == 0:  
+          queue.append(n)
+      
+    return sorted(eventual_safe_nodes)
