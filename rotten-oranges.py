add9ffbc6aab5a841790521ff0c2ea73adc5eb92
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-17 06:39:14 -0800

    Solution to 994. Rotting Oranges

--- rotten-oranges.py
+++ rotten-oranges.py
@@ -1,41 +1,43 @@
 class Solution:
   def orangesRotting(self, grid: List[List[int]]) -> int:
-    # init var to store row and column of the matrix
-    row,col = len(grid), len(grid[0])
-    # init a deck
-    queue = collections.deque([])
-    # init a var to store the number of fresh oranges
-    num_fresh_oranges = 0
+    # init a var to store rows and cols
+    rows = len(grid)
+    cols = len(grid[0])
     # init a var to store the time
     time = 0
-    # loop through the matrix
-    for i in range(col):
-      for j in range(row):
-        if grid[i][j] == 1:
-          # increment the numberof fresh oranges
+    # init a queue to store the rotten oranges
+    rotten_oranges_queue = []
+    # init a var to store the number of fresh oranges
+    num_fresh_oranges = 0
+    # loop through the grid to store the number of fresh oranges
+    for row in range(rows):
+      for col in range(cols):
+        if grid[row][col] == 1:
           num_fresh_oranges += 1
-        # if the orange is rotten
-        if grid[i][j] == 2: 
-          # store the row and column in the queue
-          queue.append((i,j))
+        
+        # store the rotten oranges in a queue
+        if grid[row][col] == 2:
+          rotten_oranges_queue.append([row, col])
     
-    # loop through the queue
-    while queue:
-      for _ in range(len(queue)):
-        # pop the rotten orange from the queue
-        i,j = queue.popleft()
-        # check for the four adjacent cells
-        for x, y in [(i + 1,j), (i - 1,j), (i,j + 1), (i,j - 1)]:
-          # if the adjacent cell has a fresh orange
-          if 0 <= x < col and 0 <= y < row and grid[x][y] == 1:
-              # make it rotten
-              grid[x][y] = 2
-              # decrement the count of fresh oranges
-              num_fresh_oranges -= 1
-              # append the rotten orange to the queue
-              queue.append((x,y))
-      # increment the result after traversing one "row" of the "tree"
+    # perform BFS
+    # loop until queue is not empty
+    while rotten_oranges_queue: # O(n)
+      # visit all elements of a queue
+      size = len(rotten_oranges_queue)
+      for _ in range(size): # O(n)
+        # pop the first element
+        i, j = rotten_oranges_queue.pop(0)
+        # if any of the four neighbors are rotten
+        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]: # O(1)
+          if 0 <= x < rows and 0 <= y < cols and grid[x][y] == 1:
+            # mark the orange as rotten
+            grid[x][y] = 2
+            # enter it in the queue
+            rotten_oranges_queue.append([x, y])
+            # decrement the number of fresh oranges
+            num_fresh_oranges -= 1
+      # increment  the time
       time += 1
-    # return res - 1 because the final iteration runs when all the oranges are rotten and there
-    # are no fresh oranges left
+    
+    # return the time or -1
     return max(0, time - 1) if num_fresh_oranges == 0 else -1
