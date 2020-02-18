40c906af4b31aa560c78e1f2338e6e6998ccf8a0
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-17 17:42:23 -0800

    Solution to 690. Employee Importance - BFS

--- employee-importance.py
+++ employee-importance.py
@@ -12,23 +12,37 @@ class Employee:
 """
 class Solution:
   def getImportance(self, employees: List['Employee'], id: int) -> int:
-    # init a var to store the importance
+    # the problem is of type, given a graph, find the total distance of all the nodes that can be rached from 
+    # the given node. Looks like the solution would be a BFS.
+    # init a var which would act as a queue
+    empl_queue = []
+    # init a var to store the total importance value
+    total_imp_value = 0
+    # init a dict to store employees as per their id
+    empl_dict = collections.defaultdict();
     
-    # init a queue, store the first employee object
+    # store the employees in the dict
+    for employee in employees:
+      empl_dict[employee.id] = employee
+    # get the employee object matching the id
+    empl_queue.append(id)
+    #print(empl_queue.id)
     
-    # init a var to store the visited employees
+    # loop through the list
+    while empl_queue: # O(N)
+      size = len(empl_queue)
+      # while queue is not empty
+      for _ in range(size):
+        # pop the first element
+        employee_id = empl_queue.pop(0)
+        print(employee_id)
+        # get employee object from employee id
+        employee = empl_dict[employee_id]
+        # add the importance value of each employee
+        total_imp_value += employee.importance
+        # add the elemet to the queue
+        for subordinate in employee.subordinates:
+          empl_queue.append(subordinate)
     
-    # loop throught the employees queue
-      
-      # pop the first element
-      
-      # check if the element is not visited
-      
-        # then insert it in the visited array
-        
-      # calcualate the importance
-      
-      # loop through the subordinate
-      
-        # add them to the queue
-      
+    # return the total importance value
+    return total_imp_value
