b04d7a87ee3b0f5bfb538844d45251427f35391f
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-21 06:32:14 -0800

    Update to 417. Pacific Atlantic Water Flow BFS

--- pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow.py
@@ -6,59 +6,37 @@ class Solution:
     self.matrix = [[]]
     # init a list to store the list of directions 
     self.directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+    self.neighbors = []
+    
+  def can_reach_ocean(matrix, is_pacific = False):
+    cells_that_can_reach_ocean = set();
+    queue = collections.dequeue([])
+    
+    if is_pacific:
+      for j in range(len(matrix[0])):
+        queue.append((0, j))
+      
+      for i in range(len(matrix)):
+        queue.append((i, 0))
+    else:
+      for j in range(len(matrix[0])):
+        queue.append((len(matrix)-1,j))
+      
+      # Initialize Queue to have all cells in the left column.
+      for i in range(len(matrix)):
+        queue.append((i,len(matrix)-1)) 
+
+      
   
   def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
-    self.matrix = matrix
-    # init a var to store the length of rows
-    self.rows = len(matrix)
-    # init a var to store the length of cols
-    self.col = len(matrix[0])
+    can_visit_matrix = [[False for _ in range len(matrix[0])] for _ in range(len(matrix))]
+    pacific_atlantic_cells = set();
     
-    # init a boolean matrix to store value if a cell can move water to Pacific ocean
-    can_move_to_pac_ocean = [[False for _ in range(self.cols)] for _ in range(self.rows)]
-    # init a boolean matrix to store value if a cell can move water to Atlantic ocean
-    can_move_to_atl_ocean = [[False for _ in range(self.cols)] for _ in range(self.rows)]
-    
-    # init a list of grid coordinates
-    grid_coordinates = []
-    
-    # perform DFS from the left edge; check if water can move from Pacific ocean to Atlantic ocean
-    # mark cells in the can_move_to_pac_ocean to 1 if the water can flow into it
-    for i in range(self.col): # O(n)
-      self.dfs(i, 0, can_move_to_atl_ocean)
-    
-    # perform DFS from the top edge; check if water can move from Pacific ocean to Atlantic ocean
-    for i in range(self.col):
-      self.dfs(0, i, can_move_to_atl_ocean)
-    
-    # perform DFS from the right edge
-    for i in range(self.rows):
-      self.dfs(i, self.cols - 1, can_move_to_pac_ocean)
-    
-    # perform DFS from the bottom edge
-    for i in range(self.rows):
-      self.dfs(self.rows - 1, i, can_move_to_atl_ocean)
-    
-    # loop through both the matrix to check if the cells match
-    return []
-    
-    
-  # dfs method
-  def dfs(self, x, y, can_move_to_ocean):
-      print(x)
-      print(y)
-      print(can_move_to_ocean)
-      print(can_move_to_ocean[0])
-      # init a list to store the visited nodes
-      can_move_to_ocean[x][y] = True
-      # for the x and y value of the cell indices
-      for direction in self.directions:
-        neighbor_x, neighbor_y = x + direction[0], y + direction[1]
-        # check if it can accept value from the four neighbors
-        if x < 0 or x > self.rows or y < 0 or y > self.cols or can_move_to_ocean[x][y] or can_move_to_ocean[x][y] < can_move_to_ocean[neighbor_x][neighbor_y]:
-          continue
-        # recurse itself
-        self.dfs(x, y, can_move_to_ocean)
+    pacific_cells = can_reach_ocean(matrix, is_pacific=True)
+    atlantic_cells = can_reach_ocean(matrix, is_atlantic=True)
       
+    for cell in pacific_cells:
+      if cell in atlantic_cells:
+        pacific_atlantic_cells.add(cell)
     
-        
+    return pacific_atlantic_cells
