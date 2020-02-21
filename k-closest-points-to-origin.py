a0a66b7ea2c89fd12f7f1c1eb43ccfb44ec21f4e
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-20 20:18:13 -0800

    Solution for 973. K Closest Points to Origin

--- k-closest-points-to-origin.py
+++ k-closest-points-to-origin.py
@@ -2,21 +2,29 @@ import heapq
 
 class Solution:
   def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-    # init a heap to store the closest points
-    heap = []
-
-    # loop through the points
-    for (x, y) in points:
-      # calculate the distance between the origin and the point
-      # the formula is x**2 + y**2 ?
-      # the - is only because python's heapq sorts elements in increasing order
-      dist = -(x*x + y*y)
-      # if the heap is full
-      if len(heap) == K:
-        # insert and pop element
-        heapq.heappushpop(heap, (dist, x, y))
+    # init a heap to store the results
+    closest_points_heap = []    
+    result = []
+    # loop through the list of points
+    for x, y in points:
+      distance = -(x * x +  y * y)
+      # if the length of heap is greater than or equal to k
+      if len(closest_points_heap) >= K:
+        # if the distance of the point is greater than the first point in the heap
+        if distance >= closest_points_heap[0][0]:
+          # then insert the point into the heap
+          # remove the first element
+          heapq.heappushpop(closest_points_heap, (distance, x, y))
+        # else
+        else:
+          # continue
+          continue
+      # else
       else:
-        # insert eement
-        heapq.heappush(heap, (dist, x, y))
-      # return the points   
-      return [(x,y) for (dist,x, y) in heap]
+        # insert the point into the heap
+        heapq.heappush(closest_points_heap, (distance, x, y))
+    # return the result
+    for point in closest_points_heap:
+      result.append([point[1], point[2]])
+    return result
+    
