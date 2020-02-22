279eef002d5e24279c9a89fe63e5fc4823c78496
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-21 18:57:06 -0800

    Solution for 417. Pacific Atlantic Water Flow

--- pacific-atlantic-water-flow.py
+++ pacific-atlantic-water-flow.py
@@ -1,16 +1,22 @@
 class Solution:
-  # https://leetcode.com/problems/pacific-atlantic-water-flow/discuss/90739/Python-DFS-bests-85.-Tips-for-all-DFS-in-matrix-question.
-  def __init__(self):
-    self.rows = 0
-    self.cols = 0
-    self.matrix = [[]]
-    # init a list to store the list of directions 
-    self.directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
-    self.neighbors = []
+  # https://leetcode.com/problems/pacific-atlantic-water-flow/discuss/90739/Python-DFS-bests-85.-Tips-for-all-DFS-in-matrix-question.  
+  def get_neighboring_cells(self, matrix, i, j):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+    neighbors = []
     
-  def can_reach_ocean(matrix, is_pacific = False):
+    for x, y in directions:
+      if 0 <= i + x < rows and 0 <= j + y < cols:
+        # Check that water can actually flow downstream.
+        if matrix[i][j] <= matrix[i + x][j + y]:
+          neighbors.append((i + x, j + y))
+    return neighbors 
+  
+  
+  def can_reach_ocean(self, matrix, is_pacific):
     cells_that_can_reach_ocean = set();
-    queue = collections.dequeue([])
+    queue = collections.deque([])
     
     if is_pacific:
       for j in range(len(matrix[0])):
@@ -20,20 +26,31 @@ class Solution:
         queue.append((i, 0))
     else:
       for j in range(len(matrix[0])):
-        queue.append((len(matrix)-1,j))
+        queue.append((len(matrix)-1, j))
       
       # Initialize Queue to have all cells in the left column.
       for i in range(len(matrix)):
-        queue.append((i,len(matrix)-1)) 
-
+        queue.append((i, len(matrix)-1)) 
+    
+    while queue:
+      x, y = queue.popleft()
+      if (x, y) in cells_that_can_reach_ocean:
+        continue
       
-  
+      cells_that_can_reach_ocean.add((x, y))
+      
+      for neighbor in self.get_neighboring_cells(matrix, x, y):
+        queue.append(neighbor)
+    
+    return cells_that_can_reach_ocean
+      
+    
   def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
-    can_visit_matrix = [[False for _ in range len(matrix[0])] for _ in range(len(matrix))]
+    can_visit_matrix = [[False for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
     pacific_atlantic_cells = set();
     
-    pacific_cells = can_reach_ocean(matrix, is_pacific=True)
-    atlantic_cells = can_reach_ocean(matrix, is_atlantic=True)
+    pacific_cells = self.can_reach_ocean(matrix, True)
+    atlantic_cells = self.can_reach_ocean(matrix, False)
       
     for cell in pacific_cells:
       if cell in atlantic_cells:
