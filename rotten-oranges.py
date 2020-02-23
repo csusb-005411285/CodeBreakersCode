6bfb4bcc42e5dc077542398263165f5ee491ce6f
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-22 18:17:51 -0800

    Solution for 994. Rotting Oranges

--- rotten-oranges.py
+++ rotten-oranges.py
@@ -1,43 +1,73 @@
 class Solution:
   def orangesRotting(self, grid: List[List[int]]) -> int:
-    # init a var to store rows and cols
-    rows = len(grid)
-    cols = len(grid[0])
-    # init a var to store the time
-    time = 0
-    # init a queue to store the rotten oranges
-    rotten_oranges_queue = []
+    # if there is only one cell in the matrix
+    if len(grid) == 1 and len(grid[0]) == 1: 
+      # return -1
+      if grid[0][0] == 1:
+        return -1
+      else:
+        return 0
+      
+    # init a queue
+    orange_queue = [] # O(n)
     # init a var to store the number of fresh oranges
     num_fresh_oranges = 0
-    # loop through the grid to store the number of fresh oranges
-    for row in range(rows):
-      for col in range(cols):
+    num_rotten_oranges = 0
+    # init a var to store the minimum number of mins. elapsed.
+    mins_elapsed = 0
+    # init a var to store directions
+    directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    # init a var to store the visited cells
+    visited = []
+    for row in range(len(grid)): # O(n)
+      for col in range(len(grid[0])): # O(n)
         if grid[row][col] == 1:
           num_fresh_oranges += 1
         
-        # store the rotten oranges in a queue
         if grid[row][col] == 2:
-          rotten_oranges_queue.append([row, col])
+          num_rotten_oranges += 1
+          orange_queue.append((row, col))
+    
+    if num_fresh_oranges == 0:
+      return 0
+    
+    if num_rotten_oranges == 0:
+      return -1
+   
+    # ahck alert
+    orange_queue[:1]
     
     # perform BFS
-    # loop until queue is not empty
-    while rotten_oranges_queue: # O(n)
-      # visit all elements of a queue
-      size = len(rotten_oranges_queue)
-      for _ in range(size): # O(n)
+    # while queue is not empty
+    while orange_queue:
+      size = len(orange_queue)
+      for _ in range(size):
         # pop the first element
-        i, j = rotten_oranges_queue.pop(0)
-        # if any of the four neighbors are rotten
-        for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]: # O(1)
-          if 0 <= x < rows and 0 <= y < cols and grid[x][y] == 1:
-            # mark the orange as rotten
-            grid[x][y] = 2
-            # enter it in the queue
-            rotten_oranges_queue.append([x, y])
-            # decrement the number of fresh oranges
-            num_fresh_oranges -= 1
-      # increment  the time
-      time += 1
-    
-    # return the time or -1
-    return max(0, time - 1) if num_fresh_oranges == 0 else -1
+        orange = orange_queue.pop(0)
+        # loop through the neighboring elements
+        for x, y in directions: # O(1)
+          neighbor_x = orange[0] + x
+          neighbor_y = orange[1] + y
+          # print('(' + str(neighbor_x) + ', ' + str(neighbor_y) + ') =>' + str(len(grid)) + ' => ' + str(len(grid[0])))
+          # if the neighbor is fresh
+          if 0 <= neighbor_x < len(grid) and 0 <= neighbor_y < len(grid[0]) and (neighbor_x, neighbor_y) not in visited:
+            print(grid[neighbor_x][neighbor_y])
+            if grid[neighbor_x][neighbor_y] == 1:
+              # then make it rotten
+              grid[neighbor_x][neighbor_y] = 2
+              # insert it in the queue
+              orange_queue.append((neighbor_x, neighbor_y))
+              # decrement the fresh oranges
+              num_fresh_oranges -= 1
+              visited.append((neighbor_x, neighbor_y))
+      # increment the var that stores the mins. elapsed
+      mins_elapsed += 1
+      
+    # if no of fresh oranges is 0
+    if num_fresh_oranges == 0:
+      # then return mins. elapsed
+      return mins_elapsed - 1
+    # else
+    else:
+      # return -1
+      return -1
