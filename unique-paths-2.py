befb8b9e5a4ea67e839b4b61cd759244a3e35987
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-02-29 17:09:13 -0800

    Second attempt at solving 63. Unique Paths II

--- unique-paths-2.py
+++ unique-paths-2.py
@@ -41,3 +41,71 @@ class Solution:
         
     # return the value of the last cell
     return unique_paths[-1][-1]
+
+# 2nd attempt
+class Solution:
+  def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    # init var to store rows and cols
+    rows = len(obstacleGrid)
+    cols = len(obstacleGrid[0])
+    # if the cell has one column and row
+    if rows == 1 and cols == 1:
+      # then return 0 if the value is 1 else return 0
+      return 1 if obstacleGrid[0][0] == 0 else 0
+    
+    if obstacleGrid[0][0] == 1: #
+      return 0
+    
+    # if the cell has one row
+    if rows == 1:
+      # if any cell has an obstacle
+      for i in range(cols):
+        if obstacleGrid[0][i] == 1: #
+          # return 0
+          return 0
+      
+      # return 1
+      return 1
+    
+    # if the cell has one column
+    if cols == 1:
+      # if any cell has an obstacle
+      for i in range(rows):
+        if obstacleGrid[i][0] == 1: #
+          # return 0
+          return 0
+    
+      # return 1
+      return 1
+      
+    
+    # init a matrix which would have the same size as the obstacleGrid
+    unique_paths = [[0 for _ in range(cols)] for _ in range(rows)]
+    
+    # init the 1st row
+    for col in range(cols): # 0(1)
+      if obstacleGrid[0][col] == 1:
+        break
+      else:
+        unique_paths[0][col] = 1
+    
+    # init the 1st col
+    for row in range(rows): # O(1)
+      # loop through the matrix
+      if obstacleGrid[row][0] == 1:
+        break
+      else: 
+        unique_paths[row][0] = 1       
+    
+    for row in range(1, rows):
+      for col in range(1, cols):
+        # if the value of cell has an obstalce
+        if obstacleGrid[row][col] == 1:
+          # then replace the value of the cell with 0
+          unique_paths[row][col] = 0
+        else:
+          # the value of a cell would be the sum of the cell above it and the cell to the left of it
+          unique_paths[row][col] = unique_paths[row - 1][col] + unique_paths[row][col - 1] 
+      
+    # return the value of the final cell
+    return unique_paths[-1][-1]
