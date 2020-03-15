8460ef4e8a9d3b4b15f989ab5eb7873ba5b9127d
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-03-14 21:40:01 -0700

    Solution to 684. Redundant Connection
    2nd attempt

--- redundant-connection.py
+++ redundant-connection.py
@@ -31,4 +31,41 @@ class Solution:
         return [x, y]
       self.parents[parent_x] = parent_y #
     return []
+  
+  # 2nd attempt
+  def __init__(self):
+    self.parents = []
+  
+  def find_vertex_with_no_children(self, vertex):
+    # if the vertex does not have a child
+    if self.parents[vertex] == 0:
+      # then return the vertex, which means the index
+      return vertex
+    return self.find(self.parents[vertex])
+    
+  
+  def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+    # if there are no edges or if there is only one edge
+    if len(edges) <= 1:
+      # then return the an empty list
+      return []
+    
+    # init a list to store the parents
+    self.parents = [0 for _ in range(len(edges))]
+    
+    # loop through edges
+    for x, y in edges:
+      # find the parent of each vertices
+      root_x = self.find_vertex_with_no_children(x-1)
+      root_y = self.find_vertex_with_no_children(y-1)
+      # if the roots do not match
+      if root_x != root_y:
+        # then make one vertex the parent of the other
+        self.parents[root_x] = root_y
+      # else
+      else:
+        # return the vertices
+        return [x, y]
+    return []
+        
     
