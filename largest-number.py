cd02960949f1907e170fe122c7ba700e1f926be9
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-03-16 19:20:48 -0700

    Solution to largest number using selection sort

--- largest-number.py
+++ largest-number.py
@@ -18,3 +18,29 @@ class Solution:
     result = ''.join(sorted_nums)
     # return the result
     return result.lstrip("0") or "0"
+
+  # 3rd attempt. Selection sort
+  def largestNumber(self, nums: List[int]) -> str:
+    if len(nums) == 0:
+      return ''
+    if len(nums) == 1:
+      return str(nums[0])
+    
+    # init a list to store the list of strings
+    str_list = list(map(str, nums))
+    # init a var to store the min value
+    max_index = 0
+    # init a var to store the results
+    results = ''
+    # loop through the list
+    for i in range(len(str_list)):
+      max_index = i
+      for j in range(i + 1, len(str_list)):
+        # compare the concatenation of two strings and sort them
+        if str_list[max_index] + str_list[j] < str_list[j] + str_list[max_index]:
+          max_index = j
+      str_list[i], str_list[max_index] = str_list[max_index], str_list[i]
+    # join the elements of the list
+    results = ''.join(str_list)
+    # check for leading 0s
+    return results.lstrip('0') or '0'
