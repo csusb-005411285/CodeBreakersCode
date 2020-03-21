600d7f34443b93745495ac4cd50df1f30f1c44fa
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-03-20 22:11:25 -0700

    Solution to Minimum path 2nd attempt

--- minimum-path-sum.py
+++ minimum-path-sum.py
@@ -26,5 +26,39 @@ class Solution:
            
     # return the value of the last cell
     return mat[-1][-1]  
+  
+  # 2nd attempt
+  def minPathSum(self, grid: List[List[int]]) -> int:
+    # if length of grid is 0
+    if len(grid) == 0:  
+      # then return empty list
+      return []
+    
+    # init a var to store the results
+    results = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+    
+    results[0][0] = grid[0][0]
+    
+    # fill values in the first row
+    for i in range(1, len(grid[0])):
+      results[0][i] = results[0][i - 1] + grid[0][i] 
+      
+    # fill values in the first col
+    for i in range(1, len(grid)):
+      results[i][0] = results[i - 1][0] + grid[i][0]
+    
+    
+    # loop through the matrix
+    # loop through the rows
+    for row in range(1, len(grid)):
+      # loop through the cols
+      for col in range(1, len(grid[0])):
+        # calculate the value of the cell
+        # min of the col before the cell and the row before the cell
+        # add it to the existing value of the cell
+        results[row][col] = min(results[row - 1][col], results[row][col - 1]) + grid[row][col]
+        
+    # return result
+    return results[-1][-1]
       
     
