82a572f9528eea8aed82b26903f97577dceb1d82
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-03-27 23:36:39 -0700

    Solution to sqrt. 2nd attempt

--- sqrtx.py
+++ sqrtx.py
@@ -12,3 +12,34 @@ class Solution:
       sqrt = (sqrt + x/sqrt) // 2
     
     return int(sqrt)
+  
+  # 2nd attempt. binary search
+  def mySqrt(self, x: int) -> int:
+    # init a var to store the mid
+    mid = 0 # O(1)
+    # init left to 1
+    left = 1 # O(1)
+    # init right to x
+    right = x # O(1)
+    
+    # while left < right
+    while left <= right:
+      # find mid
+      mid = left + (right - left)//2 # O(log n)
+      # if mid == x/mid
+      if mid == x/mid:
+        # return mid
+        return mid
+      
+      # if mid > x / mid
+      if mid > x/mid:
+        # the answer is in the left
+        right = mid - 1
+      # else
+      else:
+        # the answer is in the right
+        left = mid + 1
+        
+    # return right
+    return right
+
