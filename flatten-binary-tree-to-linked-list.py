66a8094af06cb98d60c97a066455427372e1f996
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-04-01 22:08:31 -0700

    Solution to Flatten Binary Tree to Linked List.
    2nd attempt

--- flatten-binary-tree-to-linked-list.py
+++ flatten-binary-tree-to-linked-list.py
@@ -37,3 +37,49 @@ class Solution:
       
         
       return vert
+
+  def flatten(self, root: TreeNode) -> None:
+    """
+    Do not return anything, modify root in-place instead.
+    """
+    if root is None:
+      return None
+
+    if not root.left and not root.right:
+      return root
+
+    return self.dfs(root, []) #n
+
+  # 2nd attempt
+  def dfs(self, vert, visited = []) -> TreeNode:
+    if vert in visited:
+      return
+
+    visited.append(vert)
+
+    if not vert.left and not vert.right:
+      return vert
+
+    left_child = None
+    right_child = None
+    if vert.left:
+      left_child = self.dfs(vert.left, visited) #n
+    if vert.right:
+      right_child = self.dfs(vert.right, visited) #n
+
+    if left_child:
+      last_left_node = left_child
+      while last_left_node.right:
+        last_left_node = last_left_node.right
+      
+      if last_left_node:
+        last_left_node.right = right_child
+
+    if left_child:
+      vert.right = left_child
+    else:
+      vert.right = right_child
+      
+    vert.left = None 
+
+    return vert
