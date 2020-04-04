0e505adeb0bf44b1b78665277039269196930742
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-04-03 20:07:56 -0700

    Solution to combination sum. 3rd attempt

--- combination-sum.py
+++ combination-sum.py
@@ -50,3 +50,25 @@ class Solution:
     
     for i in range(index, len(candidates)): # O(n)
       self.dfs(candidates, target - candidates[i], i, choices + [candidates[i]]) #O(n)
+  
+  # 3rd attempt
+  def __init__(self):
+    self.unique_combinations = []
+  
+  def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    if target == 0 or len(candidates) == 0:
+      return []
+    
+    self.dfs(candidates, target)
+    return self.unique_combinations
+  
+  def dfs(self, candidates, target, next_index = 0, choices = []):
+    if target == 0:
+      self.unique_combinations.append(choices)
+      return
+    
+    if target < 0:
+      return
+    
+    for i in range(next_index, len(candidates)):
+      self.dfs(candidates, target - candidates[i], i, choices + [candidates[i]])
