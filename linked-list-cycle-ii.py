2d45f9a9023f4d3de0e230c129437e8946b8e809
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-04-06 23:15:34 -0700

    Solution to Linked List Cycle II. # 2nd attempt

--- linked-list-cycle-ii.py
+++ linked-list-cycle-ii.py
@@ -44,4 +44,49 @@ class Solution:
         self.node_in_cycle = fast
         return True 
         
-    return False 
+    return False
+  
+# 2nd attempt
+class Solution:
+    def __init__(self):
+      self.node_in_cycle = None #1
+  
+    def detectCycle(self, head: ListNode) -> ListNode:
+      if not head:
+        return None
+      
+      if not head.next:
+        return None
+      
+      # if the linked list has a cycle
+      if (self.has_cycle(head)):
+        # get the first node of the cycle
+        # return the first node of the cycle
+        return self.first_node_of_cycle(head)
+      return None
+    
+    def has_cycle(self, head: ListNode) -> bool:
+      # init two pointers
+      slow = head #1
+      fast = head #1
+      # loop through the linkedlist
+      while slow and fast and fast.next: #n
+        slow = slow.next
+        fast = fast.next.next
+        # if two pointers meet 
+        if slow is fast:
+          self.node_in_cycle = slow
+          return True
+        
+      return False
+    
+    def first_node_of_cycle(self, head: ListNode) -> ListNode:
+      start = head
+      mid = self.node_in_cycle
+      # loop until two pointers meet
+      while start is not mid: #n
+        start = start.next
+        mid = mid.next
+      
+      return mid
+        
