0c703c0835d6dcd3ab3c5a11beb5b0ebb328dad1
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-04-14 20:13:29 -0700

    Solution to Asteroid collision. 3rd attempt

--- asteroid-collision.py
+++ asteroid-collision.py
@@ -1,67 +1,25 @@
+# 3rd attempt
+from collections import deque
+
 class Solution:
   def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-    if len(asteroids) == 0:
-      return []
-    
-    if len(asteroids) == 1:
-      return asteroids
-    
-    # init a var that will act as a stack
-    stack = [] # O(n)
-    # loop through the asteroids
-    for ast in asteroids: # O(n)
-      if ast < 0 and stack and stack[-1] > 0:
-        append_to_stack = True
-        while ast < 0 and stack and stack[-1] > 0: # O(n)
-          if -ast > stack[-1]:
-            stack.pop()
-          elif ast == -stack[-1]:
-            stack.pop()
-            append_to_stack = False
-            break
-          else:
-            append_to_stack = False
-            break
-        if append_to_stack:
-          stack.append(ast)
-      else:
-        stack.append(ast)
-    
-    return stack
-  
-  # 2nd attempt
-  def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-    if len(asteroids) == 0:
-      return []
-    
-    if len(asteroids) == 1:
-      return asteroids
-    
+    # init a deck
     stack = deque() #n
-    stack.append(asteroids[0])
-    
-    for ast in asteroids[1:]: #n
-      if stack and stack[-1] > 0:
-        if ast > 0:
-          stack.append(ast)
+    # loop through the list
+    for ast in asteroids: #n  
+      # if the top of the stack is positive and the incoming ast is negative
+      while stack and stack[-1] > 0 and ast < 0: #n
+        # then check if the incoming is greater 
+        if -ast > stack[-1]:
+          # then pop the element from the top of the stack
+          stack.pop()
+        # if the top of the stack and the incoming element is equal
+        elif -ast == stack[-1]:
+          stack.pop()
+          break 
         else:
-          if -ast < stack[-1]:
-            continue
-          if -ast == stack[-1]:
-            stack.pop()
-            continue
-          while stack and -ast > stack[-1] and stack[-1] > 0: #n
-            stack.pop()
-          else:
-            if stack and -ast == stack[-1]:
-              stack.pop()
-              continue
-            if len(stack) == 0:
-              stack.append(ast)
-            else:
-              if stack[-1] < 0:
-                stack.append(ast)
+          break 
       else:
         stack.append(ast)
         
-    return stack
+    return stack 
