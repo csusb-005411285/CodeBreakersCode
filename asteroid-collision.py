d7e86bde1131841a1e53c835e726a8e1617f2611
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-04-18 15:22:56 -0700

    Solution to asteroid collision

--- asteroid-collision.py
+++ asteroid-collision.py
@@ -2,48 +2,27 @@
 from collections import deque
 
 class Solution:
-  def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-    # init a deck
-    stack = deque() #n
-    # loop through the list
-    for ast in asteroids: #n  
-      # if the top of the stack is positive and the incoming ast is negative
-      while stack and stack[-1] > 0 and ast < 0: #n
-        # then check if the incoming is greater 
-        if -ast > stack[-1]:
-          # then pop the element from the top of the stack
-          stack.pop()
-        # if the top of the stack and the incoming element is equal
-        elif -ast == stack[-1]:
-          stack.pop()
-          break 
-        else:
-          break 
-      else:
-        stack.append(ast)
-        
-    return stack 
-  
-  # 5th attempt
+
+  # 6th attempt
   def asteroidCollision(self, asteroids: [int]) -> [int]:
     stack = deque()
 
-    for ast in asteroids:
-      if stack and stack[-1] > 0 and ast < 0:
-        while stack and stack[-1] > 0 and -ast > stack[-1]:
+    for i in range(len(asteroids)):
+      if stack and stack[-1] > 0 and asteroids[i] < 0:
+        while stack and -asteroids[i] > stack[-1] and stack[-1] > 0:
           stack.pop()
-          
+         
         if not stack:
-          stack.append(ast)
+          stack.append(asteroids[i])
           continue
           
-        if -ast == stack[-1]:
+        if stack[-1] < 0:
+          stack.append(asteroids[i])
+          continue
+
+        if -asteroids[i] == stack[-1]:
           stack.pop()
           continue
-          
-        if -ast > stack[-1]:
-          stack.append(ast)
       else:
-        stack.append(ast)
-    
+        stack.append(asteroids[i])
     return list(stack)
