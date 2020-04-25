194bf14dd632d2e720f953e4a367a0449fa2d6f8
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-04-24 20:32:54 -0700

    Solution to N-Queens.

--- n-queens-ae.py
+++ n-queens-ae.py
@@ -1,39 +1,29 @@
+# tc: o(n2), sc: o(n)
 class Solution:
 
     def solveNQueens(self, n: int) -> [str]:
-        # init a list to store the positions of the queens
         positions = {}
+        board = []
         self.solve_n_queens_helper(n, 0, positions)
-        result = self.format_output(positions, n)
-        return result
+        board = self.format_output(positions, n)
+        return board 
     
     def solve_n_queens_helper(self, n, row, positions):
-        # n is not 0-based and row is 0-based
-        if n == row:
+        if row == n:
             return True
 
-        col = 0
-        for col in range(n):
-            if self.is_cell_safe(positions, row, col):
-                positions[row] = Position(row, col)              
-                if self.solve_n_queens_helper(n, row + 1, positions):
+        for col in range(n): #n
+            if self.is_valid_position(row, col, positions):
+                positions[row] = [row, col]
+                if self.solve_n_queens_helper(n, row + 1, positions): #n
                     return True
 
         return False
     
-    def is_cell_safe(self, positions, row, col):
-        """
-        [0, 1]          [0, 3]
-                [1, 2]
-        [2, 1]          [2, 3]
-
-        [0, 1] [1, 2] [2, 3] = (0 - 1) == (1 - 2) == (2 - 3) = -1
-        [0, 3] [1, 2] [2, 1] = (0 + 3) == (1 + 2) == (2 + 1) = 3
-        """
-        for i in range(row):
-            if positions[i].col == col or \
-              positions[i].row - positions[i].col == row - col or \
-              positions[i].row + positions[i].col == row + col:
+    def is_valid_position(self, row, col, positions):
+        for i in range(row): #n
+            queen_pos_row, queen_pos_col = positions[i]
+            if col == queen_pos_col or row + col == queen_pos_row + queen_pos_col or queen_pos_row - queen_pos_col == row - col:
                 return False
         return True
 
@@ -41,19 +31,18 @@ class Solution:
         result = [["." for _ in range(n)] for _ in range(n)]
         formatted_res = []
         for position in positions:
-          curr_pos = positions[position]
-          result[curr_pos.row][curr_pos.col] = "Q"
+            row, col = positions[position] 
+            result[row][col] = "Q"
 
         for row in range(n): 
           col_str = ""
           for col in range(n): 
             col_str += result[row][col]
           formatted_res.append(col_str)
-
         return formatted_res
 
-class Position:
-  def __init__(self, row, col):
-    self.row = row
-    self.col = col
-    pass
+"""
+[0, 0]  [0, 1] [0, 2]
+[1, 0]  [1, 1] [1, 2]
+[2, 0]  [2, 1] [2, 2]
+"""
