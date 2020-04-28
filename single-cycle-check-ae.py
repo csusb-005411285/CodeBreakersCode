b44d37d35f0d70c40eab9025d2d7ed7f3c9e316c
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-04-27 23:27:54 -0700

    Solution to single cycle check.

--- single-cycle-check-ae.py
+++ single-cycle-check-ae.py
@@ -1,40 +1,29 @@
+# tc: o(n), sc: o(1)
 def hasSingleCycle(array):
-    # Write your code here.
-	# Since the problem is about finding a cycle, it is most likely a DFS solution
-	# we need to check if all the indexes are visited and 
-	# we return back to the starting index
-	
-	# init a var to count the number of elements visited
-	num_ele_visited = 0
-	# init a var to store the last position
-	last_pos = 0
-	# loop through the array
-	# loop until the number of jumps is equal to the number of elements
-	# in the array
-	while num_ele_visited < len(array):
-		# what if there is a cycle before we reach the last element
-		if num_ele_visited > 0 and last_pos == 0:
-			# return false
-			return False
-		# calculate the next jump
-		last_pos = next_pos(array, last_pos)
-		num_ele_visited += 1
-	# return if last position is at 0
-	return last_pos == 0
-	
-# define a method to calculate the next position
-def next_pos(array, index):
-	# Three conditions exists:
-		# next jump is less than the last index of the array	
-		# next jump is greater than the last index of the array
-		# next jump is negative
-	# init a var to store the next position
-	# next pos is equal to current index + value in that index
-	next_pos = (index + array[index]) % len(array)
-	if next_pos >= 0:
-		return next_pos
-	else:
-		return next_pos + len(array)
+    slow = 0
+    fast = 0
+    num_elements_visited = 0
+
+    while slow < len(array):
+        next_index = fast + array[fast]
+        if next_index == 0 and num_elements_visited >= 1 and num_elements_visited != len(array) - 1:
+            return False
+        if next_index < 0:
+            if -next_index >= len(array):
+                remainder = -next_index % len(array)
+                if remainder == 0:
+                    next_index = 0
+                else:
+                    next_index = len(array) - remainder
+            else:
+                next_index = len(array) + next_index
+        else:
+            if next_index >= len(array):
+                next_index = next_index % len(array)
+        num_elements_visited += 1
+        fast = next_index
+        slow += 1
+    return num_elements_visited == len(array) and fast == 0
 
 # 2nd attempt
 def hasSingleCycle(array):
