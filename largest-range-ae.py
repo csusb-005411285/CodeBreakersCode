7758bf27448b498be5a3b95af8284ba1322dfe1e
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-03 14:13:38 -0700

    Solution to largest range

--- largest-range-ae.py
+++ largest-range-ae.py
@@ -3,47 +3,35 @@ def largestRange(array):
     if len(array) == 1:
         return array + array
         
-    visited = {}
-    max_len = 0
-    visited = populate_visited(array, visited)
-    result = []
+    hash_map = {}
+    num_in_ranges = []
+    max_len = float('-inf')
+    results = []
+
+    for j in array:
+        hash_map[j] = False
+
     for i in range(len(array)):
-        forward = i
-        backward = i
-        backward_val = array[backward]
-        forward_val = array[forward]
-        '''
-        used a deque to add smaller elements to the start and larger elements to the end
-        '''
-        largest_range = deque() 
+        num_in_range = deque() 
+        num_in_range.append(array[i])
+        forward = array[i] + 1
+        backward = array[i] - 1
 
-        while backward_val in visited: 
-            if not visited[backward_val]:
-                visited[backward_val] = True
-                largest_range.appendleft(backward_val)
-            backward_val -= 1
-        '''
-        how does continue work inside a for and a while loop?
-        how does not and False work?
-        '''
-        while forward_val in visited:
-            if not visited[forward_val]:
-                visited[forward_val] = True
-                largest_range.append(forward_val)
-            forward_val += 1
+        while forward in hash_map and not hash_map[forward]: 
+            num_in_range.append(forward)
+            hash_map[forward] = True
+            forward += 1
+        
+        while backward in hash_map and not hash_map[backward]:
+            num_in_range.appendleft(backward)
+            hash_map[backward] = True
+            backward -= 1
+        
+        num_in_ranges.append(list(num_in_range))
 
-        '''
-        made a mistake when checking for max value
-        a = () is a tuple
-        a = set() is a set
-        ''' 
-        if len(largest_range) > max_len:
-           max_len = len(largest_range)
-           result = [] 
-           '''
-           when a set adds an element it always adds it to the begining
-           '''
-           result.append(largest_range[0])
-           result.append(largest_range[-1])
-
-    return result
+    for ranges in num_in_ranges:
+        if len(ranges) > max_len:
+            max_len = len(ranges)
+            results.append(ranges[0])
+            results.append(ranges[-1])
+    return results
