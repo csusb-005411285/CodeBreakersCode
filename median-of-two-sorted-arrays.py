395417cf285cb9d3d4efee06a00792e7b6a4a8ee
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-16 12:28:17 -0700

    Solution to median of two sorted arrays

--- median-of-two-sorted-arrays.py
+++ median-of-two-sorted-arrays.py
@@ -1,32 +1,38 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: [int], nums2: [int]) -> float:
-        if len(nums1) > len(nums2):
+        if len(nums2) < len(nums1):
             return self.findMedianSortedArrays(nums2, nums1)
         
-        len_num1 = len(nums1)
-        len_num2 = len(nums2)
+        l1 = len(nums1)
+        l2 = len(nums2)
 
         left = 0
-        right = len_num1 
-        while left <= right:
-            partition_x = left + (right - left) // 2
-            # + 1 is to handle the even odd cases correctly
-            partition_y = ((len_num1 + len_num2 + 1) // 2) - partition_x
+        right = len(nums1)
         
-            x = float('inf') if partition_x == len_num1 else nums1[partition_x]
-            left_x = float('-inf') if partition_x == 0 else nums1[partition_x - 1]
+        while left <= right:
+            m1 = left + ((right - left) // 2)
+            m2 = ((l1 + l2 + 1)// 2) - m1
 
-            y = float('inf') if partition_y == len_num2 else nums2[partition_y]
-            left_y = float('-inf') if partition_y == 0 else nums2[partition_y - 1]
+            val_m1 = float('inf') if m1 == l1 else nums1[m1]
+            val_m2 = float('inf') if m2 == l2 else nums2[m2]
 
-            if left_x <= y and left_y <= x:
-                if (len_num1 + len_num2)%2 == 0:
-                    return (max(left_x, left_y) + min(x, y))/2 
+            left_val_m1 = float('-inf') if m1 == 0 else nums1[m1 - 1]
+            left_val_m2 = float('-inf') if m2 == 0 else nums2[m2 - 1]
+
+            if left_val_m1 <= val_m2 and left_val_m2 <= val_m1:
+                if (l1 + l2) % 2 == 0:
+                    return (max(left_val_m1, left_val_m2) + min(val_m1, val_m2)) / 2
                 else:
-                    return max(left_x, left_y)
-            elif left_x > y:
-                right = partition_x - 1
-            else:
-                left = partition_x + 1
+                    '''
+                    made a mistake here
+                    '''
+                    return max(left_val_m1, left_val_m2) 
 
-        raise Exception('Arrays not sorted')
+            elif left_val_m1 > val_m2:
+                right = m1 - 1
+            elif left_val_m2 > val_m1:
+                left = m1 + 1
+            else:
+                continue
+        
+        raise Exception('Arrays not sorted'
