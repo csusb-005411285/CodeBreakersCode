ca63f6db444082d1a71c2977cf688d41d33f0fc4
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-25 16:46:02 -0700

    Solution to longest peak

--- longest-peak-ae.py
+++ longest-peak-ae.py
@@ -33,3 +33,62 @@ def get_path_from_peak(array, peaks):
             forward += 1
 
     return path        
+
+# 2nd attempt
+# tc: o(n), sc: o(n)
+def longestPeak(array):
+    if len(array) < 3:
+        return 0
+
+    heights = {}
+    peaks = get_peaks(array)
+
+    if len(peaks) == 0:
+        return 0
+
+    heights = populate_default_height(heights, peaks)
+    heights = get_heights(heights, array)
+    
+    return max(heights.values())
+
+def get_heights(heights, array):
+    for index in heights.keys():
+        backward = index
+        forward = index
+        height_for_index = 0
+
+        while backward > 0:
+            if array[backward - 1] < array[backward]:
+                height_for_index += 1
+                backward -= 1
+            else:
+                break
+
+        heights[index] = height_for_index + 1
+        height_for_index = 0
+
+        while forward < len(array) - 1:
+            if array[forward + 1] < array[forward]:
+                height_for_index += 1
+                forward += 1
+            else:
+                break
+
+        heights[index] += height_for_index  
+    
+    return heights
+
+def populate_default_height(heights, peaks):
+    for peak in peaks:
+        heights[peak] = 0
+    
+    return heights
+
+def get_peaks(array):
+    peaks = []
+
+    for i in range(1, len(array) - 1):
+        if array[i] > array[i - 1] and array[i] > array[i + 1]:
+            peaks.append(i)
+    
+    return peaks
