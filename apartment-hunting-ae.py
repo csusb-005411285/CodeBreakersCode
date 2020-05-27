57b986f43b409aafa9251fb6c2888d785265abd8
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-26 19:12:59 -0700

    Solution to apartment hunting

--- apartment-hunting-ae.py
+++ apartment-hunting-ae.py
@@ -1,22 +1,15 @@
 # tc: o(b2*r), sc: o(b)
 def apartmentHunting(blocks, reqs):
-    optimal_block = None
-    max_distance_to_all_reqs_from_block = {}
-    min_distance_to_all_reqs_from_all_block = float('inf')
-    min_distance_to_a_req = float('inf')
+    max_distance_to_all_reqs_from_a_block = [float('-inf') for _ in range(len(blocks))] 
 
     for i in range(len(blocks)):
-        max_distance_to_all_reqs_from_block[i] = float('-inf')
-        for j in range(len(reqs)):
-            distance = 0
-            min_distance_to_a_req = float('inf')
-            for k in range(len(blocks)):
-                if reqs[j] in blocks[k] and blocks[k][reqs[j]]:
-                    distance = abs(k - i) 
-                    min_distance_to_a_req = min(min_distance_to_a_req, distance)
-            max_distance_to_all_reqs_from_block[i] = max(max_distance_to_all_reqs_from_block[i], min_distance_to_a_req)
+        for req in reqs:
+            min_distance_to_a_req_from_a_block = float('inf')
+            for j in range(len(blocks)):
+                if req in blocks[j] and blocks[j][req]:
+                    min_distance_to_a_req_from_a_block = min(min_distance_to_a_req_from_a_block, abs(j - i))
+
+            max_distance_to_all_reqs_from_a_block[i] = max(max_distance_to_all_reqs_from_a_block[i], min_distance_to_a_req_from_a_block)
     
-        if min_distance_to_all_reqs_from_all_block > max_distance_to_all_reqs_from_block[i]:
-            min_distance_to_all_reqs_from_all_block = max_distance_to_all_reqs_from_block[i]
-            optimal_block = i
-    return optimal_block
+    min_val = min(max_distance_to_all_reqs_from_a_block)
+    return max_distance_to_all_reqs_from_a_block.index(min_val)
