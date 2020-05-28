8a784c8a66808a451b27c701c2c91259373dc075
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-28 06:57:53 -0700

    Solution to group anagrams

--- group-anagrams.py
+++ group-anagrams.py
@@ -1,27 +1,13 @@
+# tc: o(w * n log n), sc: o(wn)
 def groupAnagrams(words):
-    # Write your code here.
-	# init a hashmap
-	if len(words) == 0:
-		return []
-	if len(words) == 1:
-		return [words]
-	
-	hashmap = {} #
-	word = 0
-	sorted_word = ''
-	# loop through the list
-	for word in words:
-		# sort each word by its character
-		sorted_word = "".join(sorted(word))
-		# if the word exists in the list
-		if sorted_word in hashmap:
-			# find the index and append the word to the list in that index
-			hashmap[sorted_word].append(word)
-		# else
-		else:
-			# insert a new list with the sorted word as the key and original word
-			# as the value
-			hashmap[sorted_word] = [word] #
-		
-	# return the values of the hashmap
-	return hashmap.values()
+	hash_map = {}
+
+    for word in words:
+        sorted_word = ''.join(sorted(word))
+
+        if sorted_word not in hash_map:
+            hash_map[sorted_word] = [word]
+        else:
+            hash_map[sorted_word].append(word)
+        
+    return hash_map.values()
