1ef1ee01af7a763a78c00120bf5f62c12df58d39
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-27 18:23:37 -0700

    Solution to permutations

--- permutations.py
+++ permutations.py
@@ -13,3 +13,20 @@ class Solution:
         for i in range(len(nums)):
             self.permutations_helper(nums[:i] + nums[i + 1:], permutation + [nums[i]], permutations)
         return
+
+# tc: o(n!), sc: o(n)
+class Solution:
+    def permute(self, nums: [int]) -> [[int]]:
+        permutations = []
+        self.permute_helper(len(nums), [], nums, permutations)
+        return permutations
+
+    def permute_helper(self, target_len, curr_selection = [], choices = [], permutations = []):
+        if len(curr_selection) == target_len:
+            permutations.append(curr_selection)
+            return
+
+        for i in range(len(choices)):
+            self.permute_helper(target_len, curr_selection + [choices[i]], [] + choices[:i] + choices[i + 1:], permutations)
+
+        return
