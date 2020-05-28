b4e4f52671437bf1f76cae4fcbca55fdad2f6423
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-27 23:52:18 -0700

    Solution to merge two sorted lists

--- merge-linked-lists-ae.py
+++ merge-linked-lists-ae.py
@@ -6,30 +6,42 @@ class LinkedList:
 
 # tc: o(n), sc:
 def mergeLinkedLists(headOne, headTwo):
-    '''
-    solve only the failing test cases
-    '''
-    p1 = headOne
-    p2 = headTwo
+    l1 = headOne
+    l2 = headTwo
+    
+    if not l1 and not l2:
+        return l1
+    
+    if not l1:
+        return l2
+    
+    if not l2:
+        return l1
+    
+    p1 = l1
+    p2 = l2
     prev = None
 
     while p1 and p2:
-        if p1.value < p2.value:
-            prev = p1
-            p1 = p1.next
-        else:
-            temp_p2 = p2.next
-            p2.next = p1
-
-            if prev:
+        if p2.value <= p1.value:
+            if not prev:
+                prev = p2
+            else:
                 prev.next = p2
                 prev = prev.next
-            else:
-                prev = p2
-
+                
+            temp_p2 = p2.next
+            p2.next = p1
             p2 = temp_p2
+        else:
+            prev = p1
+            p1 = p1.next
 
     if p2:
-        prev.next = p2
-    
-    return headOne if headOne.value < headTwo.value else headTwo   
+        if p2.value <= prev.value:
+            p2.next = prev
+        else:
+            prev.next = p2
+        
+
+    return l1 if l1.value < l2.value else l2
