62297d9436192162b516483f132bc89865bb51de
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-05-30 16:53:15 -0700

    Update flatten a multilevel doubly linked list

--- flatten-a-multilevel-doubly-linked-list.py
+++ flatten-a-multilevel-doubly-linked-list.py
@@ -3,39 +3,42 @@ class Solution:
         if not head:
             return None
         
-        slow = head
-        last_child = None
-        self.flatten_helper(slow, last_child)
+        self.flatten_helper(head)
         curr_node = head
+        
+        while curr_node:
+            p(curr_node.val)
+            curr_node = curr_node.next
             
         return head
     
-    def flatten_helper(self, slow, last_child):
-        while slow and slow.next and not slow.child:
-            slow = slow.next
+    def flatten_helper(self, head):
+        ptr = head
+
+        while ptr.next and not ptr.child:
+            ptr = ptr.next
         
-        if slow.child is None and slow.next is None:
-            return slow
+        if not ptr.next and not ptr.child:
+            return ptr
         
-        last_child = self.flatten_helper(slow.child, last_child)
+        last_child_node_next_level = self.flatten_helper(ptr.child)
+        tmp_next_ptr = ptr.next
         
-        temp_next = slow.next
-        slow.next = slow.child
-        slow.child.prev = slow
-        slow.child = None
+        if last_child_node_next_level: 
+            last_child_node_next_level.next = tmp_next_ptr
+            if tmp_next_ptr:
+                tmp_next_ptr.prev = last_child_node_next_level
         
-        if last_child:
-            last_child.next = temp_next
+        ptr.next = ptr.child
+
+        if ptr and ptr.child:
+            ptr.child.prev = ptr
+            ptr.child = None
+
+        ptr = tmp_next_ptr
+        last_child_node_curr_level = ptr
+
+        while last_child_node_curr_level and last_child_node_curr_level.next:
+            last_child_node_curr_level = last_child_node_curr_level.next
         
-        if temp_next:
-            temp_next.prev = last_child
-        
-        curr_child = None
-        if last_child:
-            curr_child = last_child.next
-        
-        while curr_child and curr_child.next:
-            curr_child = curr_child.next
-            
-        return curr_child
-        
+        return last_child_node_curr_level
