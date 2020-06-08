a2a4f6b57c11274b59f8b6e60e518c2b6f37583d
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-06-08 01:26:46 -0700

    Solution for integer to roman

--- integer-to-roman.py
+++ integer-to-roman.py
@@ -1,15 +1,23 @@
 # tc: o(1), sc: o(1)
-class Solution:
     def intToRoman(self, num: int) -> str:
         thousands = ['', 'M', 'MM', 'MMM']
         hundreds = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
         tens = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
-        ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+        units = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+        roman = ''
 
-        first_digit = num//1000
-        second_digit = (num%1000)//100
-        third_digit = (num%100)//10
-        fourth_digit = num%10
+        quotient_thousands = num//1000
+        remainder_thoudsands = num%1000
+        roman += thousands[quotient_thousands]
+        
+        quotient_hundreds = remainder_thoudsands//100
+        remainder_hundreds = remainder_thoudsands%100
+        roman += hundreds[quotient_hundreds] 
+        
+        quotient_tens = remainder_hundreds//10
+        remainder_tens = remainder_hundreds%10
+        
+        roman += tens[quotient_tens]
+        roman += units[remainder_tens]
 
-        roman = thousands[first_digit] + hundreds[second_digit] + tens[third_digit] + ones[fourth_digit]
-        return roman.strip()
+        return roman
