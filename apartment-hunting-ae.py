337c1331cac57e636be3c7b7c1bbc9044e23196d
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-06-12 01:46:23 -0700

    Solution to apartment hunting

--- apartment-hunting-ae.py
+++ apartment-hunting-ae.py
@@ -13,3 +13,47 @@ def apartmentHunting(blocks, reqs):
     
     min_val = min(max_distance_to_all_reqs_from_a_block)
     return max_distance_to_all_reqs_from_a_block.index(min_val)
+
+# 2nd approach
+# tc: o(n), sc: o(n)
+def apartmentHunting(blocks, reqs):
+    distances = {}
+    max_distances_from_each_block = get_max_distances_from_each_block(blocks, reqs, distances)
+    max_distance_to_all_reqs_from_a_block = get_max_distances_to_reqs(list(zip(*list(max_distances_from_each_block.values()))))
+    min_val = min(max_distance_to_all_reqs_from_a_block)
+    return max_distance_to_all_reqs_from_a_block.index(min_val)
+
+def get_max_distances_to_reqs(distance):
+    result = []
+
+    for i in range(len(distance)):
+        result.append(max(distance[i])) 
+
+    return result
+
+
+def get_max_distances_from_each_block(blocks, reqs, distances):
+    for req in reqs:
+        distances[req] = [float('inf') for row in blocks]
+        
+        #forward sweep
+        for i in range(len(blocks)):
+            if blocks[i][req]:
+                distances[req][i] = 0
+            else:
+                if i == 0:
+                    distances[req][i] = min(float('inf'), distances[req][i])
+                else:
+                    distances[req][i] = min(distances[req][i], distances[req][i - 1] + 1)
+        
+        # backward sweep
+        for j in range(len(blocks) - 1, -1, -1):
+            if blocks[j][req]:
+                distances[req][j] = 0
+            else:
+                if j == len(blocks) - 1:
+                    distances[req][j] = min(float('inf'), distances[req][j])
+                else:
+                    distances[req][j] = min(distances[req][j], distances[req][j + 1] + 1)
+        
+    return distances
