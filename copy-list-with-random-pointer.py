a002730ee06aad1e3a03468b124af7430a5668bc
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-07-03 03:10:51 -0700

    Solution for copy list with random pointers

--- copy-list-with-random-pointer.py
+++ copy-list-with-random-pointer.py
@@ -1,43 +1,36 @@
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        p1 = head # 7
-        p2 = None 
-        p2_temp = None
-        node_map = defaultdict(Node) 
-        new_random_node = None
-        new_next_node = None
-        new_copy_node = None
-        
         if not head:
             return None
 
-        if not p1.next:
-            if p1.random == p1:
-                p2 = Node(p1.val, None, None)
-                p2.random = p2
-                return p2
-            else:
-                p2 = Node(p1.val, None, None)
-                
-            return p2
-        
-        curr_node = p1
-        while curr_node: # 1
-            node_map[curr_node] = Node(curr_node.val, None, None) # {7: N(7), 13: N(13), 11: N(11), 1: N(1)}
-            curr_node = curr_node.next # n
+        curr_node = head 
+        ll_map = defaultdict(Node)
 
-        curr_node_2 = p1 
-        while curr_node_2: # 7
-            if curr_node_2.next in node_map: # 7
-                node_map[curr_node_2].next = node_map[curr_node_2.next] # {7: N(7, N(13), None)}
-                
-            curr_node_2 = curr_node_2.next
-        
-        curr_node_rand = p1
-        while curr_node_rand:
-            if curr_node_rand.random in node_map:
-                node_map[curr_node_rand].random = node_map[curr_node_rand.random]
+        while curr_node:
+            ll_map[curr_node] = Node(curr_node.val)
+            curr_node = curr_node.next
 
-            curr_node_rand = curr_node_rand.next
+        curr_node = head
 
-        return node_map[p1]
+        while curr_node:
+            node = None
+            node_next = None
+            node_random = None
+
+            if curr_node in ll_map:
+                node = ll_map[curr_node]
+            
+            if curr_node.next in ll_map:
+                node_next = ll_map[curr_node.next]
+            
+            if curr_node.random in ll_map:
+                node_random = ll_map[curr_node.random]
+            
+            if node:
+                node.next = node_next
+                node.random = node_random
+            
+            curr_node = curr_node.next
+
+        return ll_map[head]
+
