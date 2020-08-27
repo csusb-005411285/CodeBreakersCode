8ad5d43e5143029eacdc21861c9229774c066779
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-08-27 03:11:51 -0700

    Update find-first-and-last-position-of-element-in-sorted-array.py

--- find-first-and-last-position-of-element-in-sorted-array.py
+++ find-first-and-last-position-of-element-in-sorted-array.py
@@ -21,3 +21,39 @@ class Solution:
 
       left, right = binarySearchLeft(nums, target), binarySearchRight(nums, target)
       return (left, right) if left <= right else [-1, -1]
+
+# 2nd attempt
+class Solution:
+    def searchRange(self, nums: [int], target: int) -> [int]:
+        left = self.get_left_index(nums, target)
+        right = self.get_right_index(nums, target)
+
+        return [left, right] if left <= right else [-1, -1]
+
+    def get_left_index(self, nums, target):
+        l = 0 
+        r = len(nums) - 1
+
+        while l <= r:
+            m = l + (r - l)//2
+
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return l
+
+    def get_right_index(self, nums, target):
+        l = 0 
+        r = len(nums) - 1
+
+        while l <= r:
+            m = l + (r - l)//2
+
+            if nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        
+        return r
