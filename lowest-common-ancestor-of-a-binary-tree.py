fb0bebf7aa1cf41a7e01a037f5c1aaea2fffe76b
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-09-10 18:24:20 -0700

    Update lowest-common-ancestor-of-a-binary-tree.py

--- lowest-common-ancestor-of-a-binary-tree.py
+++ lowest-common-ancestor-of-a-binary-tree.py
@@ -2,11 +2,11 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         return self.lowest_common_ancestor_helper(root, p, q)
     
-    # 3 
-    def lowest_common_ancestor_helper(self, node, p, q): # 3, 5, 1
+    
+    def lowest_common_ancestor_helper(self, node, p, q): 
         # if the node's value matches one of the two values
         if node.val == p.val or node.val == q.val:
-            return node # 5
+            return node 
         
         # base case: if node is a leaf node
         if not node.left and not node.right: 
@@ -15,11 +15,11 @@ class Solution:
         left = None
         right = None
         # move left if node exists
-        if node.left: # 5
-            left = self.lowest_common_ancestor_helper(node.left, p, q) # 5
+        if node.left: 
+            left = self.lowest_common_ancestor_helper(node.left, p, q)
         # move right if node exists
-        if node.right: # 
-            right = self.lowest_common_ancestor_helper(node.right, p, q) # 
+        if node.right: 
+            right = self.lowest_common_ancestor_helper(node.right, p, q)
         # compare the return values and return
         
         # if the left and right are None 
@@ -32,6 +32,6 @@ class Solution:
             return right
 
         if not right and left:
-            return left # 5
+            return left 
 
-        return node # 
+        return node 
