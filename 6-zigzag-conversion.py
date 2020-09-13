9d37875a5112dd2e3c6d7b28a31450ac933595b9
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-09-13 11:31:53 -0700

    Create 6-zigzag-conversion.py

---
+++ 6-zigzag-conversion.py
@@ -0,0 +1,28 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if len(s) < numRows:
+            return s
+        
+        if numRows == 1:
+            return s
+        
+        mat = [[] for _ in range(numRows)]
+        delta = -1
+        row = 0
+        zigzag_word = ''
+
+        for char in s:
+            mat[row].append(char) 
+
+            if row == 0:
+                delta = -1
+            elif row == numRows - 1:
+                delta = 1
+
+            row += -1 *delta
+        
+        for row in range(len(mat)):
+            for col in range(len(mat[row])):
+                zigzag_word += mat[row][col]
+        
+        return zigzag_word
