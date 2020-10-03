468016d8b4fb1e5ad9ed99f131178616bf04d216
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-10-03 16:23:12 -0700

    Update course-schedule.py

--- course-schedule.py
+++ course-schedule.py
@@ -1,42 +1,33 @@
-# n, n
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: [[int]]) -> bool:
-        # build an adjacency list
-        adj_list = defaultdict(list) 
-        # build an indegree list
-        indegree_list = {}
-        # init a stack to perform DFS
+        adj_list = defaultdict(list)
+        vertices_with_no_incoming_edges = []
         stack = deque()
-        # init a list to store the courses that can be completed
-        courses = []
+        indegree_graph = defaultdict(int) 
+
+        for i in range(numCourses):
+            indegree_graph[i] = 0
         
         for i in range(len(prerequisites)):
-            indegree_list[prerequisites[i][0]] = 0
-            indegree_list[prerequisites[i][1]] = 0
-
-        for i in range(len(prerequisites)): # 0, [[1, 0]]
-            adj_list[prerequisites[i][0]].append(prerequisites[i][1])
-            indegree_list[prerequisites[i][1]] += 1
+            source = prerequisites[i][1]
+            dest = prerequisites[i][0]
+            adj_list[source].append(dest) 
+            indegree_graph[dest] += 1 
         
-        for key, value in indegree_list.items():
-            if value == 0:
+        for key, val in indegree_graph.items():
+            if indegree_graph[key] == 0:
                 stack.append(key)
+                vertices_with_no_incoming_edges.append(key)
 
-        # loop until the queue is not empty
-        while len(stack) > 0:
-            # pop element from the queue
-            node = stack.pop()
-            # store the popped element in a list
-            courses.append(node)
-            # get the neighbors of the popped element from the adj. list
-            for n in adj_list[node]:
-                # for each neighbor, reduce their count by 1 in the indegree list
-                indegree_list[n] -= 1
+        while stack:
+            vert = stack.pop()
 
-                # if a node has a count of 0 in the indegree list
-                if indegree_list[n] == 0:
-                    # push it to the stack
-                    stack.append(n)
-
-            # how do we remove the node from the graph?
-        return len(courses) == len(indegree_list.keys()) 
+            if vert in adj_list:
+                for v in adj_list[vert]:
+                    indegree_graph[v] -= 1
+                
+                    if indegree_graph[v] == 0:
+                        vertices_with_no_incoming_edges.append(v)
+                        stack.append(v)
+            
+        return len(vertices_with_no_incoming_edges) == numCourses
