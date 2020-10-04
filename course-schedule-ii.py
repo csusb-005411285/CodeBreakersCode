ca640eb6b8ef241077e901e739fa0cf18b0c3f07
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-10-04 07:23:36 -0700

    Create course-schedule-ii.py

---
+++ course-schedule-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj_list = defaultdict(list)
+        vertices_with_no_incoming_edges = []
+        stack = deque()
+        indegree_graph = defaultdict(int) 
+
+        for i in range(numCourses):
+            indegree_graph[i] = 0
+
+        for i in range(len(prerequisites)):
+            source = prerequisites[i][0]
+            dest = prerequisites[i][1]
+            adj_list[source].append(dest) 
+            indegree_graph[dest] += 1 
+
+        for key, val in indegree_graph.items():
+            if indegree_graph[key] == 0:
+                stack.append(key)
+                vertices_with_no_incoming_edges.append(key)
+
+        while stack:
+            vert = stack.pop()
+
+            if vert in adj_list:
+                for v in adj_list[vert]:
+                    indegree_graph[v] -= 1
+                
+                    if indegree_graph[v] == 0:
+                        vertices_with_no_incoming_edges.append(v)
+                        stack.append(v)
+
+        return reversed(vertices_with_no_incoming_edges) if len(vertices_with_no_incoming_edges) == numCourses else []
