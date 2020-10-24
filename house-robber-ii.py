da978155b99a39c690cf53d7d1f46d08f3ed2105
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-10-24 15:27:11 -0700

    Create house-robber-ii.py

---
+++ house-robber-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0] 
+        return max(self.rob_helper(nums[0: len(nums) - 1], len(nums) - 2), self.rob_helper(nums[1:], len(nums) - 2))
+
+    def rob_helper(self, nums, i):
+        if i == 0:
+            return nums[0]
+        if i == 1:
+            return max(nums[0], nums[1])
+        return max(nums[i] + self.rob_helper(nums, i - 2), self.rob_helper(nums, i - 1))
