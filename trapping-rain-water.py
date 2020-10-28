3a4f199317c376c33f1c447f977b82ac88eb8ce3
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-10-27 18:22:43 -0700

    Update trapping-rain-water.py

--- trapping-rain-water.py
+++ trapping-rain-water.py
@@ -1,3 +1,4 @@
+# DP
 class Solution:
     def trap(self, height: [int]) -> int:
         # use two lists to store the right and left max
@@ -31,3 +32,39 @@ class Solution:
 
         # return the sum
         return sum(water_capacity)
+
+# One pass technique
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if len(height) < 3:
+            return 0
+        # use two vars to store the max left and right values
+        max_left = 0
+        max_right = 0
+        # use a var to store the volume
+        vol = 0
+        # use two pointers to loop 
+        left = 0
+        right = len(height) - 1
+        # lopp until the two pointers cross each ohter
+        while left <= right: 
+            # if the left value is less than or equal to the right value
+            if height[left] <= height[right]: 
+                # store the max left value
+                max_left = max(max_left, height[left]) 
+                # calculate the cumulative volume
+                if left != 0:
+                    vol += max_left - height[left] 
+                # increment the left pointer
+                left += 1 
+            # else
+            else:
+                # store the max right value
+                max_right = max(max_right, height[right])
+                # calculate the cumulative volume
+                if right != len(height) - 1:
+                    vol += max_right - height[right]
+                # decrement the right pointer
+                right -= 1
+        # return the val
+        return vol
