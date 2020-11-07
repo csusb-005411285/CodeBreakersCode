14f8fd0836482167b971b7c1aed896515b003753
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-11-07 07:20:17 -0800

    Update number-of-binary-tree-topologies.py

--- number-of-binary-tree-topologies.py
+++ number-of-binary-tree-topologies.py
@@ -2,13 +2,12 @@ def numberOfBinaryTreeTopologies(n, cache = {0: 1, 1: 1}):
     if n in cache:
         return cache[n]
     
-    for i in range(1, n + 1):
-        total_children = 0
-        for lc in range(i):
-            rc = i - 1 - lc
-            nlc = numberOfBinaryTreeTopologies(lc)
-            nrc = numberOfBinaryTreeTopologies(rc)
-            total_children += nlc * nrc
+	total_children = 0
+	for lc in range(n):
+		rc = n - 1 - lc
+        nlc = numberOfBinaryTreeTopologies(lc)
+        nrc = numberOfBinaryTreeTopologies(rc)
+        total_children += nlc * nrc
 
     cache[n] = total_children 
     return cache[n] 
