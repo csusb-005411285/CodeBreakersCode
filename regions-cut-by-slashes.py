ac5ad8919d97224d47ec7b0ba86b00167d16ee0a
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-11-12 07:49:42 -0800

    Create regions-cut-by-slashes.py

---
+++ regions-cut-by-slashes.py
@@ -0,0 +1,46 @@
+class Solution:
+    # the variable names are deceptive, grid is not a grid, but a list
+    def regionsBySlashes(self, grid: [str]) -> int:
+        len_grid = len(grid) * 3
+        big_grid = [['' for _ in range(len_grid)] for _ in range(len_grid)]
+        visited = set()
+        num_regions = 0
+        stack = deque()
+        # [\/, /]
+        # when building a grid * 3
+        # the row corresponds to index of the list
+        # the column corresponds to the position of the character in the string
+        # \/ and / are considered two separate strings
+        # grid is a list and not a matrix
+        for i, string in enumerate(grid): # 
+            for j, char in enumerate(string):
+                if char == '/':
+                    # start from first row and last col
+                    # keep traversing diagonally
+                    # until the last row and first col 
+                    big_grid[3 * i][(3 * j) + 2] = '/'
+                    big_grid[(3 * i) + 1][(3 * j) + 1] = '/'
+                    big_grid[(3 * i) + 2][3 * j] = '/'
+                elif char == '\\':
+                    # start from first row and first col
+                    # keep traversing diagonally
+                    # until the last row and last col 
+                    big_grid[3 * i][3 * j] = '\\'
+                    big_grid[(3 * i) + 1][(3 * j) + 1] = '\\'
+                    big_grid[(3 * i) + 2][(3 * j) + 2] = '\\'
+        
+        for i, big_string in enumerate(big_grid): # 
+            for j, char in enumerate(big_string):
+                if char == '' and (i, j) not in visited:
+                    num_regions += 1
+                    stack.append((i, j))
+                    while stack:
+                        x, y = stack.pop()
+                        if (x, y) in visited:
+                            continue
+                        visited.add((x, y))
+                        for n_x, n_y in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                            if 0 <= n_x < len_grid and 0 <= n_y < len_grid:
+                                if big_grid[n_x][n_y] == '':
+                                    stack.append((n_x, n_y))
+        return num_regions
