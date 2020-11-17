da612c6d2838d3301446c00f696f92b12270bda2
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-11-17 07:06:14 -0800

    Update search-in-rotated-sorted-array.py

--- search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array.py
@@ -1,3 +1,39 @@
+# Optimized solution. Use this over the other.
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        arr = nums
+        left = 0
+        right = len(arr) - 1
+        # Template 2
+        while left < right:
+            mid = left + ((right - left)//2)
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] > arr[right]:
+                left = mid + 1
+            else:
+                right = mid
+        pivot = left
+        
+        if arr[pivot] == target:
+            return pivot
+        elif target <= arr[-1]:
+            left = pivot
+            right = len(arr) - 1
+        elif target > arr[-1]:
+            right = pivot
+            left = 0
+
+        while left <= right:
+            mid = left + ((right - left)//2)
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if len(nums) == 1:
