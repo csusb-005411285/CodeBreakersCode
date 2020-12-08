70811fc584226e5ce1ed567a93ff493b4e68dc71
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-12-07 18:45:06 -0800

    Update k-diff-pairs-in-an-array.py

--- k-diff-pairs-in-an-array.py
+++ k-diff-pairs-in-an-array.py
@@ -1,3 +1,15 @@
+# Concise solution
+class Solution:
+    def findPairs(self, nums: [int], k: int) -> int:
+        nums_map = Counter(nums)
+        result = []
+        for i, v in nums_map.items():
+            if k == 0 and v > 1:
+                result.append(i)
+            elif k > 0 and i + k in nums_map:
+                result.append((i, i + k))
+        return len(result)
+
 # o(n), o(n)
 class Solution:
     def findPairs(self, nums: [int], k: int) -> int:
