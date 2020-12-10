fb0687d5e0bd60c01788d9915446fb26a7939f62
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-12-09 19:45:22 -0800

    Create add-one-row-to-tree.py

---
+++ add-one-row-to-tree.py
@@ -0,0 +1,27 @@
+class Solution:
+    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
+        count = d - 1
+        if d == 1:
+            new_node = TreeNode(v, root) # assign the root only to left child
+            return new_node
+        queue = deque()
+        queue.append([(root)])
+        count -= 1
+        nodes = []
+        while queue:
+            nodes = queue.popleft()
+            if count == 0:
+                break
+            neighbors = []
+            for node in nodes:
+                if node.left:
+                    neighbors.append((node.left))
+                if node.right:
+                    neighbors.append((node.right))
+            queue.append(neighbors)
+            count -= 1    
+        for node in nodes:
+            new_node = TreeNode(v, node.left, node.right)
+            node.left = TreeNode(v, node.left, None)
+            node.right = TreeNode(v, None, node.right)
+        return root
