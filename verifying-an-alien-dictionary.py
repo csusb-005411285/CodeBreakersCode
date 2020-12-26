9f09437103920bb692859ae8d745502bd9b9cc0d
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-12-25 16:12:39 -0800

    Update verifying-an-alien-dictionary.py

--- verifying-an-alien-dictionary.py
+++ verifying-an-alien-dictionary.py
@@ -1,28 +1,20 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        char_map = defaultdict(int)
+        order_map = defaultdict(int)
+        is_equal = True
         for i, char in enumerate(order):
-            char_map[char] = i
-        is_sorted = True
+            order_map[char] = i
         for i in range(len(words) - 1):
-            if words[i] == words[i + 1]: return True
-            curr_word = self.get_word_order(words[i], char_map) 
-            next_word = self.get_word_order(words[i + 1], char_map) 
-            is_sorted = self.compare_words(curr_word, next_word)
-            if not is_sorted: return False
+            is_equal = self.compare_words(words[i], words[i + 1], order_map)
+            if not is_equal:
+                return False
         return True
 
-    def compare_words(self, word1, word2):
-        words_combine = list(zip(word1, word2))
-        for i, word in enumerate(words_combine):
-            if word[0] < word[1]:
-                return True 
-            elif word[0] > word[1]:
+    def compare_words(self, word1, word2, order_map):
+        combined_words = zip(word1, word2)
+        for char1, char2 in combined_words:
+            if order_map[char1] < order_map[char2]:
+                return True
+            elif order_map[char1] > order_map[char2]:
                 return False
-        return True if len(word1) < len(word2) else False
-
-    def get_word_order(self, word, char_map):
-        word_order = []
-        for i, char in enumerate(word):
-            word_order.append(char_map[char])
-        return word_order 
+        return True if len(word1) <= len(word2) else False
