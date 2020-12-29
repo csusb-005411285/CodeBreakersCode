23033a37acba55c2063fecb1bac838af09be7890
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-12-29 09:53:18 -0800

    Update kth-smallest-element-in-a-bst.py

--- kth-smallest-element-in-a-bst.py
+++ kth-smallest-element-in-a-bst.py
@@ -10,3 +10,25 @@ class Solution:
             count -= 1
             root = node.right
         return node.val
+
+# Recursive 
+class Solution:
+    def __init__(self):
+        self.counter = 0
+        self.kth_smallest = None
+
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        self.counter = k
+        self.kth_smallest_helper(root)
+        return self.kth_smallest
+    
+    def kth_smallest_helper(self, node):
+        if self.counter == 0: return
+        if not node: return
+        self.kth_smallest_helper(node.left)
+        self.counter -= 1
+        if self.counter == 0:
+            self.kth_smallest = node.val
+            return
+        self.kth_smallest_helper(node.right)
+        return
