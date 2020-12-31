eba08fc0e2aecc0d5996a31e2968561475a98faa
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-12-31 07:29:40 -0800

    Update first-missing-positive.py

--- first-missing-positive.py
+++ first-missing-positive.py
@@ -3,7 +3,7 @@ class Solution:
         i = 0
         while i < len(nums):
             num = nums[i]
-            if 0 <= num < len(nums) and nums[i] != nums[num - 1]:
+            if 0 < num < len(nums) and nums[i] != nums[num - 1]:
                 nums[i], nums[num - 1] = nums[num - 1], nums[i]
             else:
                 i += 1
