4c7076dde68b9e22bb80ec83b5b50b327c6fc711
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2020-12-31 16:09:00 -0800

    Update merge-two-binary-trees.py

--- merge-two-binary-trees.py
+++ merge-two-binary-trees.py
@@ -1,3 +1,4 @@
+# In-order traversal
 class Solution:
     def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
         if t1 and not t2: return t1
@@ -13,4 +14,21 @@ class Solution:
         if not t1.left and left_node: t1.left = left_node
         right_node = self.merge_trees_helper(t1.right, t2.right)
         if not t1.right and right_node: t1.right = right_node
+        return t1
+
+# Post-order traversal
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        return self.merge_trees_helper(t1, t2)
+
+    def merge_trees_helper(self, t1, t2):
+        if not t1 and not t2: return None
+        if not t1 and t2: return t2
+        if t1 and not t2: return t1
+        left_child = self.merge_trees_helper(t1.left, t2.left)
+        right_child = self.merge_trees_helper(t1.right, t2.right)
+        if t1 and t2:
+            t1.val += t2.val
+        if not t1.left and left_child: t1.left = left_child
+        if not t1.right and right_child: t1.right = right_child
         return t1
