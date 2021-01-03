21f184858b97ae2a1c1d0623c09aa76042da4d2f
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-03 08:24:09 -0800

    Update three-sum-ae.py

--- three-sum-ae.py
+++ three-sum-ae.py
@@ -1,3 +1,25 @@
+# Leetcode
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        triplets = []
+        for i, num in enumerate(nums):
+            if i - 1 >= 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                if nums[left] + num + nums[right] > 0:
+                    right -= 1
+                elif nums[left] + num + nums[right] < 0:
+                    left += 1
+                else:
+                    triplets.append([nums[left], num, nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < len(nums) and left - 1 >= 0 and nums[left] == nums[left - 1]:
+                        left += 1
+        return triplets
 
 def threeNumberSum(array, targetSum):
     triplets = [] #n
