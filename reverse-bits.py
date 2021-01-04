3632fe18e0c025f02e5962ccbdf3685f8f0f32ff
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-04 06:44:32 -0800

    Update reverse-bits.py

--- reverse-bits.py
+++ reverse-bits.py
@@ -1,9 +1,10 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        reverse_bits = 0
+        reversed_bit = 0
         power = 31
         while n:
-            reverse_bits += (n & 1) << power
-            n = n >> 1
-            power = power - 1
-        return reverse_bits
+            last_bit = (n & 1)
+            reversed_bit |= last_bit << power
+            power -= 1
+            n >>= 1
+        return reversed_bit
