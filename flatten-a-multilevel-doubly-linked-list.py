6e9ab820105a0eb3c33eb4f2938c4c23a929a502
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-05 06:04:52 -0800

    Update flatten-a-multilevel-doubly-linked-list.py

--- flatten-a-multilevel-doubly-linked-list.py
+++ flatten-a-multilevel-doubly-linked-list.py
@@ -1,44 +1,29 @@
 class Solution:
     def flatten(self, head: 'Node') -> 'Node':
-        if not head:
-            return None
-        
-        self.flatten_helper(head)
-        curr_node = head
-        
-        while curr_node:
-            p(curr_node.val)
-            curr_node = curr_node.next
-            
+        if not head: return None
+        self._flatten(head)
         return head
     
-    def flatten_helper(self, head):
-        ptr = head
-
-        while ptr.next and not ptr.child:
-            ptr = ptr.next
-        
-        if not ptr.next and not ptr.child:
-            return ptr
-        
-        last_child_node_next_level = self.flatten_helper(ptr.child)
-        tmp_next_ptr = ptr.next
-        
-        if last_child_node_next_level: 
-            last_child_node_next_level.next = tmp_next_ptr
-            if tmp_next_ptr:
-                tmp_next_ptr.prev = last_child_node_next_level
-        
-        ptr.next = ptr.child
-
-        if ptr and ptr.child:
-            ptr.child.prev = ptr
-            ptr.child = None
-
-        ptr = tmp_next_ptr
-        last_child_node_curr_level = ptr
-
-        while last_child_node_curr_level and last_child_node_curr_level.next:
-            last_child_node_curr_level = last_child_node_curr_level.next
-        
-        return last_child_node_curr_level
+    def _flatten(self, node):
+        curr = node
+        next_level_last_node = None
+        while curr and curr.next:
+            if curr.child: break
+            curr = curr.next
+        # Pay attention to this step. 
+        # Base case
+        if curr.next is None and curr.child is None: return curr    
+        if curr.child:
+            next_level_last_node = self._flatten(curr.child)   
+        curr_next = curr.next
+        if next_level_last_node:
+            next_level_last_node.next = curr_next
+            if curr_next:
+                curr_next.prev = next_level_last_node
+        if curr.child:
+            curr.next = curr.child
+            curr.child.prev = curr
+            curr.child = None
+        while curr.next:
+            curr = curr.next
+        return curr
