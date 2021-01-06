0d0a298d467cfe380f00b1c6bf4c5bfdddd66945
Vigneshwaran Santhalingam <34956791+vnesh-san@users.noreply.github.com>
2021-01-06 12:38:19 -0800

    adding 3 new files
    add selection, insertion sort and finding a corresponding node in clone of BT

---
+++ find-a-corresponding-node-of-BT-in-a-clone.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        # In-Order Traversal of both trees
+
+        stack_original, stack_clone = deque([]), deque([])
+        node_original, node_clone = original, cloned
+
+        while stack_original or node_original:
+            while node_original:
+                stack_original.append(node_original)
+                stack_clone.append(node_clone)
+                node_original = node_original.left
+                node_clone = node_clone.left
+
+            cur_original = stack_original.pop()
+            cur_clone = stack_clone.pop()
+
+            if cur_original is target:
+                return cur_clone
+
+            node_original = cur_original.right
+            node_clone = cur_clone.right
