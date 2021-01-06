28fe8619ab227b6f1f1757c8c273b3a013001b86
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-06 07:17:19 -0800

    Update leftmost-column-with-at-least-a-one.py

--- leftmost-column-with-at-least-a-one.py
+++ leftmost-column-with-at-least-a-one.py
@@ -1,6 +1,17 @@
 class Solution:
     def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
         rows, cols = binaryMatrix.dimensions()
+        row = 0
+        col = cols - 1
+        while 0 <= row < rows and 0 <= col < cols:
+            element = binaryMatrix.get(row, col)
+            if element == 0: row += 1
+            if element == 1: col -= 1
+        return col + 1 if row != -1 and col != cols - 1 else -1
+
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        rows, cols = binaryMatrix.dimensions()
         curr_row, curr_col = 0, cols-1
         while curr_row < rows and curr_col >= 0:
             if not binaryMatrix.get(curr_row, curr_col):
