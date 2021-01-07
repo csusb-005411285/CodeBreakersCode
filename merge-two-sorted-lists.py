b9b9c351ee2f6322d6b3b8e2af63b8e6445c8157
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-07 07:33:11 -0800

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -1,5 +1,34 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        curr = None
+        ptr1 = l1
+        ptr2 = l2
+        if ptr1 is None and ptr2 is None: return None
+        if ptr1 and ptr2 is None: return ptr1
+        if ptr2 and ptr1 is None: return ptr2
+        while ptr1 and ptr2:
+            if ptr1.val <= ptr2.val:
+                ptr1_next = ptr1.next
+                if curr is None:
+                    curr = ptr1
+                else:
+                    curr.next = ptr1
+                    curr = curr.next
+                ptr1 = ptr1_next
+            else:
+                ptr2_next = ptr2.next
+                if curr is None:
+                    curr = ptr2
+                else:
+                    curr.next = ptr2
+                    curr = curr.next
+                ptr2 = ptr2_next
+        if ptr2 is not None: curr.next = ptr2
+        if ptr1 is not None: curr.next = ptr1
+        return l1 if l1.val <= l2.val else l2
+    
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         if not l1 or not l2:
             return l1 or l2
 
