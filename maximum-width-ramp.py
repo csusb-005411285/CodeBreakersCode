d922ba314d798891f3542dbef514dba6d8559f08
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-07 19:08:34 -0800

    Update maximum-width-ramp.py

--- maximum-width-ramp.py
+++ maximum-width-ramp.py
@@ -1,4 +1,21 @@
 class Solution:
+    def maxWidthRamp(self, A: List[int]) -> int:
+        a = A
+        index_list = []
+        value_map = defaultdict(int)
+        min_index = float('inf')
+        max_width = 0
+        for i, val in enumerate(a):
+            value_map[i] = val
+        sorted_values = dict(sorted(value_map.items(), key=lambda item: item[1]))
+        min_index = float('inf')
+        for index, value in sorted_values.items():
+            max_width = max(max_width, index - min_index)
+            if index < min_index:
+                min_index = index
+        return max_width
+    
+class Solution:
     def maxWidthRamp(self, A: [int]) -> int:
         a = A
         max_ramp = 0 
