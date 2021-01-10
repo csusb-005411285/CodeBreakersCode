e32438e836e46f7d7fd0b752b6fb40757ad5badf
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-10 10:45:19 -0800

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -1,31 +1,21 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        curr = None
-        ptr1 = l1
-        ptr2 = l2
-        if ptr1 is None and ptr2 is None: return None
-        if ptr1 and ptr2 is None: return ptr1
-        if ptr2 and ptr1 is None: return ptr2
-        while ptr1 and ptr2:
+        ptr1, ptr2, dummy_head = l1, l2, ListNode(float('inf'))
+        curr = dummy_head
+        while ptr1 is not None and ptr2 is not None:
             if ptr1.val <= ptr2.val:
-                ptr1_next = ptr1.next
-                if curr is None:
-                    curr = ptr1
-                else:
-                    curr.next = ptr1
-                    curr = curr.next
-                ptr1 = ptr1_next
+                curr.next = ptr1
+                curr = ptr1
+                ptr1 = ptr1.next
             else:
-                ptr2_next = ptr2.next
-                if curr is None:
-                    curr = ptr2
-                else:
-                    curr.next = ptr2
-                    curr = curr.next
-                ptr2 = ptr2_next
-        if ptr2 is not None: curr.next = ptr2
-        if ptr1 is not None: curr.next = ptr1
-        return l1 if l1.val <= l2.val else l2
+                curr.next = ptr2
+                curr = ptr2
+                ptr2 = ptr2.next
+        if ptr1 is not None:
+            curr.next = ptr1
+        if ptr2 is not None:
+            curr.next = ptr2
+        return dummy_head.next
     
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
