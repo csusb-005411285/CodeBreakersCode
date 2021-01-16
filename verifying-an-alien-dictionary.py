69ca1f292bd08c4cdafb312258cd72d67643e094
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-01-16 09:02:43 -0800

    Update verifying-an-alien-dictionary.py

--- verifying-an-alien-dictionary.py
+++ verifying-an-alien-dictionary.py
@@ -1,5 +1,24 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_pos = defaultdict(int)
+        for position, char in enumerate(order):
+            order_pos[char] = position
+        for i in range(0, len(words) - 1):
+            for char1, char2 in zip(words[i], words[i + 1]):
+                if char1 == char2:
+                    continue
+                else:
+                    if order_pos[char1] < order_pos[char2]:
+                        break
+                    else:
+                        return False
+            else:
+                if len(words[i]) > len(words[i + 1]):
+                    return False
+        return True
+                    
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
         order_map = defaultdict(int)
         is_equal = True
         for i, char in enumerate(order):
