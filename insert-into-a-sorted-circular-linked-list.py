aba0d4d4607fbeee370ea0b01340bc89cf50ec6b
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-02-21 11:33:28 -0800

    Update insert-into-a-sorted-circular-linked-list.py

--- insert-into-a-sorted-circular-linked-list.py
+++ insert-into-a-sorted-circular-linked-list.py
@@ -6,32 +6,23 @@ class Solution:
             return new_node
         if head.next is head:
             new_node = Node(insertVal)
-            if insertVal < head.val:
-                new_node.next = head
-                head.next = new_node
-            else:
-                head.next = new_node
-                new_node.next = head
+            new_node.next = head
+            head.next = new_node
             return head
-        curr = head.next
         prev = head
+        curr = head.next
         found = False
         while True:
             if prev.val <= insertVal <= curr.val:
                 found = True
-            elif prev.val > curr.val:
-                if insertVal <= curr.val:
-                    found = True
-                elif insertVal >= prev.val:
-                    found = True
-            if found:
-                new_node = Node(insertVal)
-                prev.next = new_node
-                new_node.next = curr
-                return head
-            prev, curr = curr, curr.next
-            if prev == head:
                 break
-        # for cases like [3, 3, 3, 3] 0
+            if prev.val > curr.val:
+                if insertVal >= prev.val or insertVal <= curr.val:
+                    found = True
+                    break
+            prev = prev.next
+            curr = curr.next
+            if prev is head:
+                break
         prev.next = Node(insertVal, curr)
         return head
