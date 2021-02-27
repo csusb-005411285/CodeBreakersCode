149c980aa3ad33b923c1da7ea5f09c18cd8b162d
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-02-27 14:05:46 -0800

    Update interval-list-intersections.py

--- interval-list-intersections.py
+++ interval-list-intersections.py
@@ -1,4 +1,25 @@
 class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        i = 0 
+        j = 0
+        if not firstList or not secondList:
+            return []
+        result = []
+        while i < len(firstList) and j < len(secondList):
+            intersect = []
+            startTime = max(firstList[i][0], secondList[j][0])
+            endTime = min(firstList[i][1], secondList[j][1])
+            if not endTime < startTime:
+                intersect.append(startTime)
+                intersect.append(endTime)
+                result.append(intersect)
+            if firstList[i][1] > secondList[j][1]:
+                j += 1
+            else:
+                i += 1
+        return result
+    
+class Solution:
     def intervalIntersection(self, A: [[int]], B:[[int]]) -> [[int]]:
         res = []
         a = A
