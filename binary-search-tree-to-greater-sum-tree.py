6bfb20abecf641b20fb68d9a0c9b346e4c7df0de
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-02-28 15:31:26 -0800

    Update binary-search-tree-to-greater-sum-tree.py

--- binary-search-tree-to-greater-sum-tree.py
+++ binary-search-tree-to-greater-sum-tree.py
@@ -1,10 +1,20 @@
 class Solution:
+    def __init__(self):
+        self.prefix_sum = 0
+        
     def bstToGst(self, root: TreeNode) -> TreeNode:
-        self.helper(root, 0)
+        self._bst_to_gst(root)
         return root
     
-    def helper(self, node, num):
-        if node is None:
-            return num
-        node.val += self.helper(node.right, num)
-        return self.helper(node.left, node.val)
+    def _bst_to_gst(self, node):
+        if not node:
+            return
+        if node and not node.left and not node.right:
+            self.prefix_sum += node.val
+            node.val = self.prefix_sum
+            return
+        self._bst_to_gst(node.right)
+        self.prefix_sum += node.val
+        node.val = self.prefix_sum
+        self._bst_to_gst(node.left)
+        return 
