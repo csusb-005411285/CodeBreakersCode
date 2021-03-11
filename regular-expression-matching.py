5c394cf97560d39b8e39f2312c8e753052f3092d
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-03-10 19:02:56 -0800

    Update regular-expression-matching.py

--- regular-expression-matching.py
+++ regular-expression-matching.py
@@ -2,16 +2,18 @@ class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         cache = [[False for _ in range(len(p) + 1)] for _ in range(len(s) + 1)]
         cache[0][0] = True
-        for j in range(2, len(cache[0])):
-            if p[j - 1] == '*':
-                cache[0][j] = cache[0][j - 2]
-        for i in range(1, len(cache)):
-            for j in range(1, len(cache[0])):
-                if s[i - 1] == p[j - 1] or p[j - 1] == '.':
-                    cache[i][j] = cache[i - 1][j - 1]
-                elif p[j - 1] == '*':
-                    if p[j - 2] != s[i - 1] and p[j - 2] != '.':
-                        cache[i][j] = cache[i][j - 2]
-                    else:
-                        cache[i][j] = cache[i - 1][j] | cache[i][j - 1] | cache[i][j - 2]
+        for col in range(2, len(cache[0])):
+            if p[col - 1] == '*':
+                cache[0][col] = cache[0][col - 2] 
+        for row in range(1, len(cache)):
+            for col in range(1, len(cache[0])):
+                if s[row - 1] == p[col - 1]:
+                    cache[row][col] = cache[row - 1][col - 1]
+                else:
+                    if p[col - 1] == '.':
+                        cache[row][col] = cache[row - 1][col - 1]
+                    elif p[col - 1] == '*':
+                        cache[row][col] = cache[row][col - 2]
+                        if s[row - 1] == p[col - 2] or p[col - 2] == '.':
+                            cache[row][col] |= cache[row - 1][col]
         return cache[-1][-1]
