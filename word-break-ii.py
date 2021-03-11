968ff08bab7a1252f2ded713e594e3cd464a2329
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-03-11 07:33:22 -0800

    Update word-break-ii.py

--- word-break-ii.py
+++ word-break-ii.py
@@ -1,6 +1,26 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         cache = defaultdict(list)
+        self._word_break(s, wordDict, cache, 0)
+        return cache[0]
+    
+    def _word_break(self, s, words, cache, index):
+        if index in cache:
+            return cache[index]
+        if index >= len(s):
+            return ['']
+        for i, word in enumerate(words):
+            if s[index: index + len(word)] == word:
+                for segment in self._word_break(s, words, cache, index + len(word)):
+                    new_word = word + ' ' + segment if segment != '' else word
+                    cache[index].append(new_word)
+        if index not in cache:
+            cache[index] = []
+        return cache[index]
+    
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        cache = defaultdict(list)
         self._word_break(s, wordDict, cache)
         return cache[s]
     
