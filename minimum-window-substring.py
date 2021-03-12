65fcbc6d5f1853e74f5f42bfa29b2871669bdfeb
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-03-12 07:44:48 -0800

    Update minimum-window-substring.py

--- minimum-window-substring.py
+++ minimum-window-substring.py
@@ -1,22 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if s == t: return t
         char_map = Counter(t)
+        char_found = 0
+        subsequence = s
         left = 0
-        substr_found = 0
-        substr = s
         for right, char in enumerate(s):
             if char in char_map:
                 char_map[char] -= 1
-                if char_map[char] == 0: substr_found += 1
-            while substr_found == len(char_map.keys()) and left <= right:
-                if len(s[left: right + 1]) < len(substr):
-                    substr = s[left: right + 1]
-                if s[left] not in char_map:
-                    left += 1
-                    continue
-                else:
+                if char_map[char] == 0:
+                    char_found += 1
+            while char_found == len(char_map) and left <= right:
+                if len(subsequence) > right - left + 1:
+                    subsequence = s[left: right + 1]
+                if s[left] in char_map:
+                    if char_map[s[left]] == 0:
+                        char_found -= 1
                     char_map[s[left]] += 1
-                    if char_map[s[left]] > 0: substr_found -= 1
                     left += 1
-        return substr if left != 0 and right != len(s) else ''
+                while left < len(s) and s[left] not in char_map:
+                    left += 1
+        return subsequence if left != 0 and right != len(s) else ''
