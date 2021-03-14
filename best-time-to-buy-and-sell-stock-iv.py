5ca7de152e406ddf6c716a70f15493849356e3ab
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-03-13 16:30:52 -0800

    Update best-time-to-buy-and-sell-stock-iv.py

--- best-time-to-buy-and-sell-stock-iv.py
+++ best-time-to-buy-and-sell-stock-iv.py
@@ -1,5 +1,21 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
+        if k * 2 >= len(prices): # can this be 4?
+            profit = 0
+            for i in range(len(prices) - 1):
+                if prices[i + 1] >= prices[i]:
+                    profit += prices[i + 1] - prices[i]
+            return profit
+        profits = [0] * len(prices)
+        for trans in range(k):
+            profit = 0
+            for i, price in enumerate(prices[1:], start = 1):
+                profit = max(profits[i], profit + prices[i] - prices[i - 1])
+                profits[i] = max(profits[i - 1], profit)
+        return profits[-1]
+        
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
         if k == 0 or len(prices) <= 1:
             return 0
         if k * 2 >= len(prices): 
