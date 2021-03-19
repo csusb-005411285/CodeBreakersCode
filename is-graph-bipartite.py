d46143f12d1ce3c02a43e850abaeb20f636ce1a1
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-03-18 19:44:24 -0700

    Update is-graph-bipartite.py

--- is-graph-bipartite.py
+++ is-graph-bipartite.py
@@ -1,21 +1,32 @@
 class Solution:
-    def isBipartite(self, graph: List[List[int]]) -> bool:
-        vertex_colors = [0 for i in range(len(graph))]
-        is_bipartite = True
-        for k, v in enumerate(graph):
-            is_bipartite = self.is_bipartite_helper(graph, k, vertex_colors)
-            if not is_bipartite:
-                return False
-        return is_bipartite
+    def __init__(self):
+        self.found_verts_with_same_color = False
     
-    def is_bipartite_helper(self, graph, vert, vertex_colors):
-        if vertex_colors[vert] == 0:
-            vertex_colors[vert] = 1
-        for neigh in graph[vert]:
-            if vertex_colors[neigh] == vertex_colors[vert]:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        adj_list = defaultdict(set)
+        vert_list = [0] * len(graph)
+        self.build_adj_list(graph, adj_list)
+        for vert in adj_list.keys():
+            if not self._is_bipartite(adj_list, vert_list, vert):
                 return False
-            if vertex_colors[neigh] == -vertex_colors[vert]:
-                continue
-            vertex_colors[neigh] = -vertex_colors[vert]
-            self.is_bipartite_helper(graph, neigh, vertex_colors)
+        return True
+    
+    def build_adj_list(self, graph, adj_list):
+        for source, destinations in enumerate(graph):
+            for dest in destinations:
+                adj_list[source].add(dest)
+                adj_list[dest].add(source)
+    
+    def _is_bipartite(self, adj_list, vert_list, vert):
+        if self.found_verts_with_same_color:
+            return False
+        if vert_list[vert] == 0:
+            vert_list[vert] = 1
+        for neighbor in adj_list[vert]:
+            if vert_list[vert] == vert_list[neighbor]:
+                self.found_verts_with_same_color = True
+                return False
+            if vert_list[neighbor] == 0:
+                vert_list[neighbor] = -vert_list[vert]
+                self._is_bipartite(adj_list, vert_list, neighbor)
         return True
