534ca9b3c4690e2f271569b00b9cf8e7ba79e2e0
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-04-05 08:01:12 -0700

    Update subarray-sum-equals-k.py

--- subarray-sum-equals-k.py
+++ subarray-sum-equals-k.py
@@ -1,5 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        prefix_sum = [0] * len(nums)
+        count = 0
+        num_map = defaultdict(int)
+        num_map[0] = 1
+        for i, num in enumerate(nums):
+            prefix_sum[i] = prefix_sum[i - 1] + num if i - 1 >= 0 else num
+        for i, num in enumerate(prefix_sum):
+            if num - k in num_map:
+                count += num_map[num - k]
+            num_map[num] += 1
+        return count
+    
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
         nums_sums = []
         subarray_sums = {0: 1}
         count = 0
