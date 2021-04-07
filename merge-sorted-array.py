980340aefb4184ea4d63fba6e48259617e5ea32e
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-04-06 19:24:01 -0700

    Update merge-sorted-array.py

--- merge-sorted-array.py
+++ merge-sorted-array.py
@@ -3,20 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        ptr1 = len(nums1) - len(nums2) - 1
-        ptr2 = len(nums2) - 1
-        insert_at = len(nums1) - 1
-        while ptr1 >= 0 and ptr2 >= 0:
-            if nums2[ptr2] >= nums1[ptr1]:
-                nums1[insert_at] = nums2[ptr2]
-                insert_at -= 1
-                ptr2 -= 1
+        p1 = m - 1
+        p2 = n - 1
+        p3 = m + n - 1
+        while p1 >= 0 and p2 >= 0:
+            if p1 >= p3:
+                break
+            if nums1[p1] > nums2[p2]:
+                nums1[p1], nums1[p3] = nums1[p3], nums1[p1]
+                p1 -= 1
+                p3 -= 1
             else:
-                nums1[insert_at], nums1[ptr1] = nums1[ptr1], nums1[insert_at]
-                insert_at -= 1
-                ptr1 -= 1
-        while ptr2 >= 0 and insert_at >= 0:
-            nums1[insert_at] = nums2[ptr2]
-            insert_at -= 1
-            ptr2 -= 1
+                nums1[p3] = nums2[p2]
+                p3 -= 1
+                p2 -= 1
+        while p2 >= 0:
+            nums1[p3] = nums2[p2]
+            p2 -= 1
+            p3 -= 1
         return nums1
