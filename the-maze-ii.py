39cb16b5197985d416270546b9350dd64d0fa4d1
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-04-12 07:33:29 -0700

    Update the-maze-ii.py

--- the-maze-ii.py
+++ the-maze-ii.py
@@ -1,29 +1,28 @@
 class Solution:
     def shortestDistance(self, maze: List[List[int]], start: List[int], destination: List[int]) -> int:
         heap = []
-        shortest_dist = - 1
-        heappush(heap, (0, start))
+        cache = [[float('inf') for _ in range(len(maze[0]))] for _ in range(len(maze))]
+        dist = - 1
         visited = set()
-        cache = [[0 for _ in range(len(maze[0]))] for _ in range(len(maze))]
+        heappush(heap, (0, start))
         while heap:
             dist, node = heappop(heap)
             row, col = node
-            visited.add((row, col))
             cache[row][col] = dist
-            if row == destination[0] and col == destination[1]:
+            if node == destination:
                 return dist
-            for n_row, n_col in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                neigh_row = row
-                neigh_col = col
-                d = 0
-                while 0 <= neigh_row + n_row < len(maze) and 0 <= neigh_col + n_col < len(maze[0]) and maze[neigh_row + n_row][neigh_col + n_col] != 1:
-                    neigh_row += n_row
-                    neigh_col += n_col
+            if (row, col) in visited:
+                continue
+            visited.add((row, col))
+            for x, y in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                n_row = row
+                n_col = col
+                d = dist
+                while 0 <= n_row + x < len(maze) and 0 <= n_col + y < len(maze[0]) and maze[n_row + x][n_col + y] != 1: #
+                    n_row += x
+                    n_col += y
                     d += 1
-                if neigh_row < 0 or neigh_row >= len(maze) or neigh_col < 0 or neigh_col >= len(maze[0]):
-                    continue
-                if (neigh_row, neigh_col) not in visited or cache[neigh_row][neigh_col] > dist + d:
-                    cache[neigh_row][neigh_col] = dist + d
-                    heappush(heap, (dist + d, (neigh_row, neigh_col)))
-        return shortest_dist
-        
+                if (n_row, n_col) not in visited or cache[n_row][n_col] > d:
+                    cache[n_row][n_col] = d
+                    heappush(heap, (d, [n_row, n_col]))
+        return -1
