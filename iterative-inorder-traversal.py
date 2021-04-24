52542e353b4789bf9f03b156f74ebce37e0c0c47
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-04-24 13:26:04 -0700

    Update iterative-inorder-traversal.py

--- iterative-inorder-traversal.py
+++ iterative-inorder-traversal.py
@@ -1,3 +1,17 @@
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = []
+        res = []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+		
 def iterativeInOrderTraversal(tree, callback):
 	ptr = tree
     prev = None
