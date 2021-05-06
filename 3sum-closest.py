5677bc6f20aa9373d43e3bd218cc047bc2672796
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-05-06 07:54:54 -0700

    Update 3sum-closest.py

--- 3sum-closest.py
+++ 3sum-closest.py
@@ -1,27 +1,21 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1 
-        nums = sorted(nums)
-        closest_sum = float('inf')
-
-        # 1,1,1,0
-        # 0,1,1,1
-        for i in range(len(nums) - 1): # 0
-            left = i + 1 # 1
-            right = len(nums) - 1 # 3
-
-            while left < right: # 2 < 3
-                sum_num = nums[left] + nums[i] + nums[right] # 1 + 0 + 1 = 2
-
-                if abs(target - sum_num) < abs(closest_sum): # (100 - (2)) < 98 
-                    closest_sum = target - sum_num # 98
-                
-                if sum_num > target: # 2 > 100
-                    right -= 1 # 
-                elif sum_num < target: # 2 < 100
-                    left += 1 # 3
-                elif sum_num == target:
-                    return sum_num
-                
-        return  target - closest_sum 
+        nums.sort()
+        closest = float('inf')
+        i = 0
+        for i in range(len(nums)):
+            while i - 1 >= 0 and i < len(nums) and nums[i] == nums[i - 1]:
+                i += 1
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                three_sum = nums[i] + nums[left] + nums[right] 
+                if abs(target - three_sum) < abs(target - closest):
+                    closest = three_sum
+                if three_sum < target:
+                    left += 1 
+                elif three_sum > target:
+                    right -= 1
+                else:
+                    return three_sum
+        return closest
