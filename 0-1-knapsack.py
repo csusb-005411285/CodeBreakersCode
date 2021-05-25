95202d77410035126ec8115717a1cfbe3880a711
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-05-25 06:34:31 -0700

    Create 0-1-knapsack.py

---
+++ 0-1-knapsack.py
@@ -0,0 +1,16 @@
+class Solution:
+    def solve(self, weights, values, capacity):
+        cache = [[-1 for _ in range(capacity + 1)] for _ in range(len(weights))]
+        return self._solve(weights, values, capacity, 0, cache)
+
+    def _solve(self, weights, values, capacity, i, cache):
+        if i >= len(weights) or capacity <= 0:
+            return 0
+        if cache[i][capacity] != -1:
+            return cache[i][capacity]
+        profit1 = 0
+        if weights[i] <= capacity:
+            profit1 = values[i] + self._solve(weights, values, capacity - weights[i], i + 1, cache)
+        profit2 = self._solve(weights, values, capacity, i + 1, cache)
+        cache[i][capacity] = max(profit1, profit2) 
+        return cache[i][capacity]
