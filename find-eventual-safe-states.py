2d78b1eff471e776a16b5f6d02141c53f13bcfcb
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-06-19 17:25:43 -0700

    Update find-eventual-safe-states.py

--- find-eventual-safe-states.py
+++ find-eventual-safe-states.py
@@ -31,3 +31,27 @@ class Solution:
                         stack.append(v)
 
         return sorted(vertices_with_no_outgoing_edges) 
+
+# DFS
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]: 
+        visited = [False] * len(graph)
+        in_process =[False] * len(graph)
+        safe_nodes = []
+        for i in range(len(graph)):
+            if not dfsUtils(i, graph, visited, in_process):
+              safe_nodes.append(i)
+        return safe_nodes
+    
+    def dfsUtils(self, node, g, visited, in_process):
+        if in_process[node]:
+            return True
+        if visited[node]:
+          return False
+        in_process[node] = True
+        visited[node] = True
+        for adj_node in g[node]:
+          if self.dfsUtils(adj_node, g, visited, in_process):
+            return True
+        in_process[node] = False
+        return False
