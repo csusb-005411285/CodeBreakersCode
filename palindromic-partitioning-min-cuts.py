badf1cf9b58f73185fab903ac877e595f438d605
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-07-04 07:36:38 -0700

    Update palindromic-partitioning-min-cuts.py

--- palindromic-partitioning-min-cuts.py
+++ palindromic-partitioning-min-cuts.py
@@ -1,3 +1,67 @@
+# top-down with caching
+class Solution:
+    def minCut(self, s: str) -> int:
+        cache = defaultdict(int)
+        palindrome_cache = defaultdict(int)
+        return self._min_cuts(s, 0, len(s) - 1, cache, palindrome_cache)
+    
+    def _min_cuts(self, s, start, end, cache, palindrome_cache):
+        if start == end:
+            return 0
+        if self.is_palindrome(s, start, end, palindrome_cache):
+            return 0
+        if (start, end) in cache:
+            return cache[(start, end)]
+        min_cuts = len(s) - 1
+        for i in range(start, end):
+            if self.is_palindrome(s, start, i, palindrome_cache):
+                min_cuts = min(min_cuts, 1 + self._min_cuts(s, i + 1, end, cache, palindrome_cache))
+        cache[(start, end)] = min_cuts
+        return min_cuts
+    
+    def is_palindrome(self, s, start, end, palindrome_cache):
+        if (start, end) in palindrome_cache:
+            return palindrome_cache[(start, end)]
+        while start <= end:
+            if s[start] != s[end]:
+                palindrome_cache[(start, end)] = False
+                return False
+            start += 1
+            end -= 1
+        palindrome_cache[(start, end)] = True
+        return True
+
+# bottom-up with caching
+  class Solution:
+    def minCut(self, s: str) -> int:
+        cache = [float('inf') for _ in range(len(s))]
+        cache[0] = 0
+        palindrome_cache = defaultdict(int)
+        for i in range(len(s)):
+            for j in range(len(s)):
+                self.is_palindrome(s, j, i, palindrome_cache)
+                
+        for i in range(1, len(s)):
+            for j in range(i, -1, -1):
+                if self.is_palindrome(s, j, i, palindrome_cache):
+                    if j - 1 >= 0:
+                        cache[i] = min(cache[i], cache[j - 1] + 1)
+                    else:
+                        cache[i] = 0
+        return cache[-1]
+    
+    def is_palindrome(self, s, start, end, palindrome_cache):
+        if (start, end) in palindrome_cache:
+            return palindrome_cache[(start, end)]
+        while start < end:
+            if s[start] != s[end]:
+                palindrome_cache[(start, end)] = False
+                return False
+            start += 1
+            end -= 1
+        palindrome_cache[(start, end)] = True
+        return True
+
 # brute force
 class Solution:
     def minCut(self, s: str) -> int:
