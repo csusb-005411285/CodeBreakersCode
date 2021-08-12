156c03a8592d5ead29e42a297c7651c5e5eba4eb
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-08-12 16:36:03 -0700

    Update split-array-largest-sum.py

--- split-array-largest-sum.py
+++ split-array-largest-sum.py
@@ -1,23 +1,39 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
+        # init vars
         left = max(nums)
         right = sum(nums)
-        ans = -1
-        while left <= right:
-            mid = left + ((right - left)//2)
-            if self.can_split_array_into_less_than_m_parts(nums, m, mid):
-                right = mid - 1
-                ans = mid
+        # check for invalid inputs
+        if m == 1:
+            return sum(nums) # 3
+        # binary search
+        while left < right:
+            mid = left + (right - left)//2
+            if self.get_number_of_splits(nums, mid) <= m:
+                right = mid # 1
             else:
                 left = mid + 1
-        return ans
+        # return
+        return right
     
-    def can_split_array_into_less_than_m_parts(self, nums, m, num):
+    def get_number_of_splits(self, nums, limit):
+        # init vars
         count = 0
-        sums = 0
+        _sum = 0
+        # loop, process
         for i, val in enumerate(nums):
-            sums += val
-            if sums > num:
+            # sum
+            _sum += val
+            # check if sum > val
+            if _sum > limit:
+                # incr. count
                 count += 1
-                sums = val
-        return count + 1 <= m # + 1 is for the last partition
+                # reset sum
+                _sum = val
+        return count + 1 # 2
+        
+'''
+1. if the number of splits are less than or equal to k, then we need to check the left half. This is because the smaller the number, the more number of splits we can get.
+2. + 1 is for the last split.
+3. if m == 1, it means the array is not split. So, the max value is sum of all values. 
+'''
