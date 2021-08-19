a6e71c83ba8a076a35a40b83d2c77897b10ff9af
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-08-18 17:37:57 -0700

    Update range-module.py

--- range-module.py
+++ range-module.py
@@ -1,48 +1,55 @@
-class RangeModule:
 
     def __init__(self):
-        self.map = defaultdict(int)
+        self.ranges = defaultdict(int)
 
     def addRange(self, left: int, right: int) -> None:
-        # 1. check for overlap
-        start, end = left, right
-        start_idx = []
-        for prev_start, prev_end in self.map.items():
-            if start <= prev_end and prev_start <= end:
-                start = min(start, prev_start)
-                end = max(end, prev_end)
-                start_idx.append(prev_start)
-        while start_idx:
-            del self.map[start_idx.pop()]
-        # 2. insert interval
-        self.map[start] = end
+        # init vars
+        key = left
+        val = right
+        start_times = []
+        # loop
+        for start, end in self.ranges.items():
+            if key <= end and start <= val:
+                # update start and end times
+                key = min(key, start)
+                val = max(val, end)
+                # get start time of overlapping interval
+                start_times.append(start)
+        # remove existing interval
+        while start_times:
+            start = start_times.pop()
+            del self.ranges[start]
+        # add new interval 
+        self.ranges[key] = val
 
     def queryRange(self, left: int, right: int) -> bool:
-        for start, end in self.map.items(): # 10, 14
-            if start <= left and right <= end: # 13 <= 14 and 10 <= 15
+        # loop
+        for start, end in self.ranges.items():
+            # check if left and right are within any interval ranges
+            if start <= left and right <= end:
                 return True
         return False
+            
 
     def removeRange(self, left: int, right: int) -> None:
-        # 1. find overlapping ranges
-        start, end = left, right
-        start_idx = []
-        for prev_start, prev_end in self.map.items():
-            if start <= prev_end and prev_start <= end:
-                start = min(start, prev_start)
-                end = max(end, prev_end)
-                start_idx.append(prev_start)
-        # 2. delete interval
-        while start_idx:
-            del self.map[start_idx.pop()]
-        # 3. add new intervals
-        if start < left:
-            self.map[start] = left
-        if end > right:
-            self.map[right] = end
-            
-# Your RangeModule object will be instantiated and called as such:
-# obj = RangeModule()
-# obj.addRange(left,right)
-# param_2 = obj.queryRange(left,right)
-# obj.removeRange(left,right)
+        # vars
+        key = left
+        val = right
+        start_times = []
+        # loop
+        for start, end in self.ranges.items():
+            if key <= end and start <= val:
+                # update start and end times
+                key = min(key, start)
+                val = max(val, end)
+                # get start time of overlapping interval
+                start_times.append(start)
+        # remove existing interval
+        while start_times:
+            start = start_times.pop()
+            del self.ranges[start]
+        # add new interval
+        if key < left:
+            self.ranges[key] = left
+        if right < val:
+            self.ranges[right] = val
