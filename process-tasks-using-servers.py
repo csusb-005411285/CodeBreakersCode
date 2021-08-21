8e5e88112b27d83c8a4b2c3fdc34a8afc0d0b850
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-08-21 13:19:29 -0700

    Create process-tasks-using-servers.py

---
+++ process-tasks-using-servers.py
@@ -0,0 +1,63 @@
+class Solution:
+    def assignTasks(self, servers: List[int], tasks: List[int]) -> List[int]:
+        # a heap to hold the servers 
+        server_heap = []
+        # a heap to hold the busy servers
+        busy_heap = []
+        # a queue to hold the tasks
+        tasks_deque = deque()
+        # a list to store the results
+        ans = []
+        # a variable to hold the time
+        time = 0
+        # to keep track of tasks
+        offset = 0
+        # add the servers to the heap
+        # use a tuple of server weight, server id
+        for _id, weight in enumerate(servers):
+            heappush(server_heap, (weight, _id))
+        tasks = deque(tasks)
+        # loop until we have processed all the tasks
+        while len(ans) < len(tasks):
+            # bring all tasks that are ready to run to the task queue
+            # if the index of the task list is smaller than the time so far, add it to the task queue
+            offset = len(ans) + len(tasks_deque) # 1.
+            for i in range(offset, len(tasks)):
+                if i <= time:
+                    tasks_deque.append(tasks[i])
+                else:
+                    break
+                    
+            # add busy servers that are now free to the servers list
+            # check the server at the top of the heap to find out its time.
+            # if the time is smaller than the current time
+            # add it to the free servers heap
+            while busy_heap and busy_heap[0][0] <= time:
+                _, weight, _id = heappop(busy_heap)
+                heappush(server_heap, (weight, _id))
+                
+            # if no servers are free
+            # set the time to  the time at the top of the busy servers heap
+            # continue
+            # This step is to prevent TLE.
+            if not server_heap:
+                time = busy_heap[0][0]
+                continue
+            
+            # as long as there are tasks in the task queue and servers in the servers heap
+            # get task from task queue
+            # get server from top of heap
+            # add server index to result set
+            # add server to busy heap. add a tuple containing (current time + time to complete the task, server weight, server index)
+            while tasks_deque and server_heap:
+                task_time = tasks_deque.popleft()
+                weight, _id = heappop(server_heap)
+                ans.append(_id)
+                heappush(busy_heap, (time + task_time, weight, _id))
+            time += 1
+        # return ans
+        return ans    
+    
+'''
+1. We use this len(ans) + len(tasks_deque) because each task will either be already processed or will be ready for processing. However, we need the tasks that are not yet processed. 
+'''
