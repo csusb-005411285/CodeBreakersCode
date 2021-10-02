511eb5fe8a9b3515d9a7e1da4e86836793990596
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-10-02 14:54:18 -0700

    Update stone-game-III.py

--- stone-game-III.py
+++ stone-game-III.py
@@ -1,5 +1,30 @@
 class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
+        cache = defaultdict(int)
+        res = self._stone_game(stoneValue, 0, cache)
+        if res > 0:
+            return 'Alice'
+        elif res < 0:
+            return 'Bob'
+        return 'Tie'
+    
+    def _stone_game(self, nums, i, cache):
+        if i in cache:
+            return cache[i]
+        if i >= len(nums):
+            return 0
+        option2 = float('-inf')
+        option3 = float('-inf')
+        option1 = nums[i] - self._stone_game(nums, i + 1, cache)
+        if i + 1 < len(nums):
+            option2 = nums[i] + nums[i + 1] - self._stone_game(nums, i + 2, cache)
+        if i + 2 < len(nums):
+            option3 = nums[i] + nums[i + 1] + nums[i + 2] - self._stone_game(nums, i + 3, cache)
+        cache[i] = max(option1, option2, option3)
+        return cache[i]
+    
+class Solution:
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
         cache = [float('-inf') for _ in range(len(stoneValue) + 1)]
         cache[-1] = 0
         for i in range(len(stoneValue) - 1, -1, -1):
