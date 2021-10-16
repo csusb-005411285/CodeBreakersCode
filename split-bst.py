617cb645df4b253ba5fb69fa3d4c2f8be9b74cae
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-10-16 14:00:04 -0700

    Update split-bst.py

--- split-bst.py
+++ split-bst.py
@@ -1,15 +1,19 @@
 class Solution:
     def splitBST(self, root: Optional[TreeNode], target: int) -> List[Optional[TreeNode]]:
-        return self.split_bst(root, target)
-        
-    def split_bst(self, node, target):
+        return self.split(root, target)
+    
+    def split(self, node, target):
         if not node:
             return [None, None]
-        if target >= node.val:
-            root_smaller_nodes, root_greater_nodes = self.split_bst(node.right, target)
-            node.right = root_smaller_nodes
-            return [node, root_greater_nodes]
+        if node.val <= target:
+            root_tree_smaller_curr_node, root_tree_greater_curr_node = self.split(node.right, target)
+            node.right = root_tree_smaller_curr_node
+            return [node, root_tree_greater_curr_node] # 1.
         else:
-            root_smaller_nodes, root_greater_nodes = self.split_bst(node.left, target)
-            node.left = root_greater_nodes
-            return [root_smaller_nodes, node]
+            root_tree_smaller_curr_node, root_tree_greater_curr_node = self.split(node.left, target)
+            node.left = root_tree_greater_curr_node
+            return [root_tree_smaller_curr_node, node] # 1.
+        
+'''
+1. Do not return node.left or node.right. The tree is being built at every step.
+'''
