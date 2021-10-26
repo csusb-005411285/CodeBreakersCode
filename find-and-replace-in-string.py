aefabe863c1ccdda82c69132d9c2470f526d694b
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-10-26 15:03:00 -0700

    Update find-and-replace-in-string.py

--- find-and-replace-in-string.py
+++ find-and-replace-in-string.py
@@ -1,6 +1,36 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
         # init vars
+        replaced_string = []
+        index_map = defaultdict(int)
+        indices_set = set(indices)
+        # initial checks
+        
+        # process
+        # 0 1 2 3
+        # a b c d
+        # s = "abcd", indices = [0, 2], sources = ["a", "cd"], targets = ["eee", "ffff"]
+        for i, val in enumerate(indices): # 1, 2
+            index_map[val] = i # {0: 0, 2: 1}
+        # abcd  
+        i = 0
+        while i < len(s): # 2 c
+            char = s[i]
+            if i in indices_set: # 2
+                val = index_map[i] # 1
+                substr = sources[val] # 'cd'
+                if s[i: i + len(substr)] == substr: # s[2: 4] == cd
+                    replaced_string.append(targets[val]) # ['eee', 'b', 'ffff']
+                    i += len(substr) # 2 + 2 = 4
+                    continue
+            replaced_string.append(char)
+            i += 1
+        # return
+        return ''.join(replaced_string)
+    
+class Solution:
+    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+        # init vars
         string = list(s)
         # inital checks 
         
