ab8e0f15c2b0c8d8af90437aeecd665c6e90fb92
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-11-01 15:40:11 -0700

    Update my-calendar-i.py

--- my-calendar-i.py
+++ my-calendar-i.py
@@ -1,6 +1,37 @@
 class MyCalendar:
 
     def __init__(self):
+        self.events = []
+
+    def book(self, start: int, end: int) -> bool:
+        if not self.events:
+            self.events.append((start, end)) 
+            return True
+        indx = self.find_index(start) 
+        if indx - 1 >= 0 and self.events[indx - 1][1] > start:
+            return False
+        if (indx < len(self.events) and end > self.events[indx][0]):
+            return False
+        if indx == len(self.events):
+            self.events.append((start, end))
+        else:
+            self.events.insert(indx, (start, end))
+        return True
+    
+    def find_index(self, target): 
+        left = 0
+        right = len(self.events) - 1 
+        while left <= right: 
+            mid = left + (right - left)//2
+            if self.events[mid][0] <= target: 
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left 
+    
+class MyCalendar:
+
+    def __init__(self):
         self.calendar = defaultdict(int)
 
     def book(self, start: int, end: int) -> bool:
