2b76d9cfd9e8ad11173149b45466435dda7522b8
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-11-01 19:42:46 -0700

    Update shortest-path-in-a-grid-with-obstacles-elimination.py

--- shortest-path-in-a-grid-with-obstacles-elimination.py
+++ shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,3 +1,49 @@
+# Dijkastra's algorithm
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        # init vars
+        heap= []
+        visited = set()
+        cache = defaultdict(int)
+        min_cost = float('inf')
+        # process
+        # add a tuple to heap. Tuple should contain ((row, col), cost, k value)
+        heappush(heap, (0, (0, 0), k)) 
+        # add source to visited
+        visited.add((0, 0, k)) 
+        cache[(0, 0, 0)] = 0
+        # set the default value of cache to  float inf
+        while heap:
+            # pop from heap
+            cost, vert, new_k = heappop(heap) 
+            row, col = vert 
+            # if value of k is less than 0
+            if new_k < 0:
+                continue
+            # if we are at the destination
+            if row == len(grid) - 1 and col == len(grid[0]) - 1: 
+                return cost
+            # get neighbors
+            for n_row, n_col in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                updated_k = new_k
+                new_cost = cost
+                if 0 <= n_row < len(grid) and 0 <= n_col < len(grid[0]): 
+                    # if neighbring cell is 1, add 1 to cost and decrement k by 1
+                    if grid[n_row][n_col] == 1:
+                        updated_k -= 1 
+                    # calculate the cost
+                    new_cost += 1
+                    # if neighbor is not visited or cost of visting neighbor is smaller than the current value
+                    if (n_row, n_col, updated_k) not in visited or ((n_row, n_col, updated_k) in cache and  cache[(n_row, n_col, updated_k)] > new_cost):
+                        # add to heap
+                        heappush(heap, (new_cost, (n_row, n_col), updated_k))
+                        # add to visited
+                        visited.add((n_row, n_col, updated_k))
+                        # add to cache
+                        cache[(n_row, n_col, updated_k)] = new_cost
+        # return
+        return -1
+
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         # init vars
