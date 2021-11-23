fe0d825d757cd1232df49d113a39fb5d548a0e4f
Ajay Kotian <45015337+csusb-005411285@users.noreply.github.com>
2021-11-23 08:12:07 -0800

    Update longest-increasing-path-in-a-matrix.py

--- longest-increasing-path-in-a-matrix.py
+++ longest-increasing-path-in-a-matrix.py
@@ -1,28 +1,25 @@
 class Solution:
     def __init__(self):
-        self.longest_increasing_path = 1
-        
+        self.longest_path = 1
+    
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        cache = [[1 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
         visited = set()
-        cache = [[1 for col in range(len(matrix[0]))] for row in range(len(matrix))]
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
-                self._longest_increasing_path(matrix, (row, col), visited, cache)
-        return self.longest_increasing_path
+                self._longest_increasing_path(matrix, cache, (row, col))
+        return self.longest_path
     
-    def _longest_increasing_path(self, matrix, vert, visited, cache):
+    def _longest_increasing_path(self, matrix, cache, vert):
         row, col = vert
         if cache[row][col] != 1:
             return cache[row][col]
-        if vert in visited:
-            return cache[row][col]
-        visited.add(vert)
-        for neighbor in [(row - 1, col), (row + 1, col), (row, col + 1), (row, col - 1)]:
-            n_row, n_col = neighbor
+        for n_row, n_col in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
             if 0 <= n_row < len(matrix) and 0 <= n_col < len(matrix[0]):
-                if (n_row, n_col) not in visited and matrix[n_row][n_col] < matrix[row][col]:
-                    cache[row][col] = max(cache[row][col], self._longest_increasing_path(matrix, (n_row, n_col), visited, cache) + 1)
-                    if cache[row][col] > self.longest_increasing_path:
-                        self.longest_increasing_path = cache[row][col]
-        visited.remove(vert)
+                if matrix[n_row][n_col] < matrix[row][col]:
+                    path = self._longest_increasing_path(matrix, cache, (n_row, n_col))
+                    cache[row][col]  = max(cache[row][col], path + 1)
+                    if cache[row][col] > self.longest_path:
+                        self.longest_path = cache[row][col]
         return cache[row][col]
+        
